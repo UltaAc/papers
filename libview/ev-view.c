a68dd453d1c5d605fa090b9ab612d225c8a4c62e
Qiu Wenbo <qiuwenbo@kylinos.com.cn>
2023-12-14 13:03:25 +0800

    libview, shell: Fix the return type of GObject new function
    For better gobject introspection support.
    
    Signed-off-by: Qiu Wenbo <qiuwenbo@kylinos.com.cn>

--- libview/ev-view.c
+++ libview/ev-view.c
@@ -8319,14 +8319,10 @@ adjustment_value_changed_cb (GtkAdjustment *adjustment,
 		view_update_range_and_current_page (view);
 }
 
-GtkWidget*
+EvView *
 ev_view_new (void)
 {
-	GtkWidget *view;
-
-	view = g_object_new (EV_TYPE_VIEW, NULL);
-
-	return view;
+	return g_object_new (EV_TYPE_VIEW, NULL);
 }
 
 static void
