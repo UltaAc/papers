UltaAc/papers

commit 4e8b39747471321d835db792a551973b63f02c8b

shell: reorganize actions for PpsWindow
Before this commit, all the PpsWindow actions had the "win"
prefix. However, not all of them had global shortcuts in
PpsApplication. It turns out, many of these actions do not correspond
to the window, but to individual documents in the document view. For
now, these are actually the same. There is one document per Papers
process. In out work towards fixing that, we need to make sure that
the actions belong to each document. This should also simplify the
split between PpsWindow and PpsDocumentView that we are working
towards.

shell/pps-window.c
@@ -426,10 +426,10 @@ pps_window_update_actions_sensitivity (PpsWindow *pps_window)
         /* Edit menu */
 	pps_window_set_action_enabled (pps_window, "select-all", has_pages &&
 				      can_get_text && !start_view_mode);
-	pps_window_set_action_enabled (pps_window, "find", can_find &&
-				      !start_view_mode);
-	pps_window_set_action_enabled (pps_window, "toggle-find", can_find &&
-				      !start_view_mode);
+	gtk_widget_action_set_enabled (GTK_WIDGET (pps_window), "doc.find",
+				       can_find && !start_view_mode);
+	gtk_widget_action_set_enabled (GTK_WIDGET (pps_window), "doc.toggle-find",
+				       can_find && !start_view_mode);
 	pps_window_set_action_enabled (pps_window, "add-annotation", can_annotate &&
 				      !start_view_mode);
 	pps_window_set_action_enabled (pps_window, "highlight-annotation", can_annotate &&
@@ -489,12 +489,10 @@ pps_window_update_actions_sensitivity (PpsWindow *pps_window)
 					has_pages &&
 					pps_view_has_selection (view) &&
 					!start_view_mode);
-	pps_window_set_action_enabled (pps_window, "find-next",
-				      has_pages && can_find_in_page &&
-				      !start_view_mode);
-	pps_window_set_action_enabled (pps_window, "find-previous",
-				      has_pages && can_find_in_page &&
-				      !start_view_mode);
+	gtk_widget_action_set_enabled (GTK_WIDGET (pps_window), "doc.find-next",
+				      has_pages && can_find_in_page && !start_view_mode);
+	gtk_widget_action_set_enabled (GTK_WIDGET (pps_window), "doc.find-previous",
+				      has_pages && can_find_in_page && !start_view_mode);
 	pps_window_set_action_enabled (pps_window, "dual-odd-left", dual_mode &&
 				      has_pages && !start_view_mode);
 
@@ -524,11 +522,11 @@ pps_window_update_actions_sensitivity (PpsWindow *pps_window)
 		pps_window_set_action_enabled (pps_window, "select-page", FALSE);
 	}
 
-	pps_window_set_action_enabled (pps_window, "go-back-history",
+	gtk_widget_action_set_enabled (GTK_WIDGET (pps_window), "doc.go-back-history",
 				      !pps_history_is_frozen (priv->history) &&
 				      pps_history_can_go_back (priv->history) &&
 				      !start_view_mode);
-	pps_window_set_action_enabled (pps_window, "go-forward-history",
+	gtk_widget_action_set_enabled (GTK_WIDGET (pps_window), "doc.go-forward-history",
 				      !pps_history_is_frozen (priv->history) &&
 				      pps_history_can_go_forward (priv->history) &&
 				      !start_view_mode);
@@ -3918,7 +3916,6 @@ pps_window_cmd_find (GSimpleAction *action,
 	PpsWindow *pps_window = user_data;
 	PpsWindowPrivate *priv = GET_PRIVATE (pps_window);
 	g_autofree gchar *selected_text = NULL;
-	gboolean is_toggled;
 
 	view = PPS_VIEW (priv->view);
         selected_text = pps_view_get_selected_text (view);
@@ -3929,13 +3926,8 @@ pps_window_cmd_find (GSimpleAction *action,
 		gtk_editable_select_region (GTK_EDITABLE (entry), 0, -1);
 		pps_window_show_find_bar (pps_window, TRUE);
 	} else {
-		is_toggled = g_variant_get_boolean (g_action_group_get_action_state (G_ACTION_GROUP (pps_window),
-										     "toggle-find"));
-
-		g_action_group_change_action_state (G_ACTION_GROUP (pps_window),
-						    "toggle-find",
-						    g_variant_new_boolean (!is_toggled));
-        }
+		gtk_widget_activate_action (GTK_WIDGET (pps_window), "doc.toggle-find", NULL);
+	}
 }
 
 static void
@@ -5224,7 +5216,6 @@ pps_window_show_find_bar (PpsWindow *pps_window,
 
 	gtk_search_bar_set_search_mode (GTK_SEARCH_BAR (priv->search_bar), TRUE);
 	gtk_widget_grab_focus (priv->search_box);
-	g_action_group_change_action_state (G_ACTION_GROUP (pps_window), "toggle-find", g_variant_new_boolean (TRUE));
 	g_action_group_change_action_state (G_ACTION_GROUP (pps_window), "show-side-pane",
 						g_variant_new_boolean (TRUE));
 	if (restart) {
@@ -5248,7 +5239,6 @@ pps_window_close_find_bar (PpsWindow *pps_window)
 
 	gtk_search_bar_set_search_mode (GTK_SEARCH_BAR (priv->search_bar), FALSE);
 	gtk_widget_grab_focus (priv->view);
-	g_action_group_change_action_state (G_ACTION_GROUP (pps_window), "toggle-find", g_variant_new_boolean (FALSE));
 
 	pps_history_thaw (priv->history);
 }
@@ -5521,14 +5511,6 @@ static const GActionEntry actions[] = {
 	{ "go-next-page", pps_window_cmd_go_next_page },
 	{ "go-first-page", pps_window_cmd_go_first_page },
 	{ "go-last-page", pps_window_cmd_go_last_page },
-	{ "go-forward", pps_window_cmd_go_forward },
-	{ "go-backwards", pps_window_cmd_go_backwards },
-	{ "go-back-history", pps_window_cmd_go_back_history },
-	{ "go-forward-history", pps_window_cmd_go_forward_history },
-	{ "find", pps_window_cmd_find },
-	{ "toggle-find", NULL, NULL, "false", pps_window_cmd_toggle_find },
-	{ "find-next", pps_window_cmd_edit_find_next },
-	{ "find-previous", pps_window_cmd_edit_find_previous },
 	{ "select-page", pps_window_cmd_focus_page_selector },
 	{ "dual-page", NULL, NULL, "false", pps_window_cmd_dual },
 	{ "dual-odd-left", NULL, NULL, "false", pps_window_cmd_dual_odd_pages_left },
@@ -5567,6 +5549,17 @@ static const GActionEntry actions[] = {
 	{ "remove-annot", pps_window_popup_cmd_remove_annotation }
 };
 
+static const GActionEntry doc_actions[] = {
+	{ "go-forward", pps_window_cmd_go_forward },
+	{ "go-backwards", pps_window_cmd_go_backwards },
+	{ "go-back-history", pps_window_cmd_go_back_history },
+	{ "go-forward-history", pps_window_cmd_go_forward_history },
+	{ "find", pps_window_cmd_find },
+	{ "toggle-find", NULL, NULL, "false", pps_window_cmd_toggle_find },
+	{ "find-next", pps_window_cmd_edit_find_next },
+	{ "find-previous", pps_window_cmd_edit_find_previous },
+};
+
 static void
 sidebar_links_link_activated_cb (void *sidebar_links, PpsLink *link, PpsWindow *window)
 {
@@ -5591,9 +5584,9 @@ history_changed_cb (PpsHistory *history,
 {
 	PpsWindowPrivate *priv = GET_PRIVATE (window);
 
-	pps_window_set_action_enabled (window, "go-back-history",
+	gtk_widget_action_set_enabled (GTK_WIDGET (window), "doc.go-back-history",
 				      pps_history_can_go_back (priv->history));
-	pps_window_set_action_enabled (window, "go-forward-history",
+	gtk_widget_action_set_enabled (GTK_WIDGET (window), "doc.go-forward-history",
 				      pps_history_can_go_forward (priv->history));
 }
 
@@ -5895,7 +5888,7 @@ do_action_named (PpsWindow *window, PpsLinkAction *action)
 	} else if (g_ascii_strcasecmp (name, "GoToPage") == 0) {
 		g_action_group_activate_action (G_ACTION_GROUP (window), "select-page", NULL);
 	} else if (g_ascii_strcasecmp (name, "Find") == 0) {
-		g_action_group_activate_action (G_ACTION_GROUP (window), "find", NULL);
+		gtk_widget_activate_action (GTK_WIDGET (window), "doc.find", NULL);
 	} else if (g_ascii_strcasecmp (name, "Close") == 0) {
 		g_action_group_activate_action (G_ACTION_GROUP (window), "close", NULL);
 	} else if (g_ascii_strcasecmp (name, "Print") == 0) {
@@ -6437,6 +6430,7 @@ static void
 pps_window_init (PpsWindow *pps_window)
 {
 	GtkEventController *controller;
+	GSimpleActionGroup *group;
 	guint page_cache_mb;
 	gboolean allow_links_change_zoom;
 	PpsWindowPrivate *priv = GET_PRIVATE (pps_window);
@@ -6498,6 +6492,14 @@ pps_window_init (PpsWindow *pps_window)
 	g_action_map_add_action_entries (G_ACTION_MAP (pps_window),
 					 actions, G_N_ELEMENTS (actions),
 					 pps_window);
+	group = g_simple_action_group_new ();
+	g_action_map_add_action_entries (G_ACTION_MAP (group),
+					 doc_actions,
+					 G_N_ELEMENTS (doc_actions),
+					 pps_window);
+	gtk_widget_insert_action_group (GTK_WIDGET (pps_window),
+					"doc", G_ACTION_GROUP (group));
+
 
 	pps_page_selector_set_model (PPS_PAGE_SELECTOR (priv->page_selector),
 		priv->model);

shell/help-overlay.ui
@@ -194,25 +194,25 @@
             <child>
               <object class="GtkShortcutsShortcut">
                 <property name="title" translatable="yes" context="shortcut window">Previous Page (Fast)</property>
-                <property name="action-name">win.go-backwards</property>
+                <property name="accelerator">&lt;Shift&gt;Page_Up</property>
               </object>
             </child>
             <child>
               <object class="GtkShortcutsShortcut">
                 <property name="title" translatable="yes" context="shortcut window">Next Page (Fast)</property>
-                <property name="action-name">win.go-forward</property>
+                <property name="accelerator">&lt;Shift&gt;Page_Down</property>
               </object>
             </child>
             <child>
               <object class="GtkShortcutsShortcut">
                 <property name="title" translatable="yes" context="shortcut window">Previous Visited Page</property>
-                <property name="action-name">win.go-back-history</property>
+                <property name="accelerator">&lt;alt&gt;P Back</property>
               </object>
             </child>
             <child>
               <object class="GtkShortcutsShortcut">
                 <property name="title" translatable="yes" context="shortcut window">Next Visited Page</property>
-                <property name="action-name">win.go-forward-history</property>
+                <property name="accelerator">&lt;alt&gt;N Forward</property>
               </object>
             </child>
             <child>
@@ -306,13 +306,13 @@
             <child>
               <object class="GtkShortcutsShortcut">
                 <property name="title" translatable="yes" context="shortcut window">Find Next</property>
-                <property name="action-name">win.find-next</property>
+                <property name="accelerator">&lt;Ctrl&gt;G F3</property>
               </object>
             </child>
             <child>
               <object class="GtkShortcutsShortcut">
                 <property name="title" translatable="yes" context="shortcut window">Find Previous</property>
-                <property name="action-name">win.find-previous</property>
+                <property name="accelerator">&lt;Ctrl&gt;&lt;Shift&gt;G &lt;Shift&gt;F3</property>
               </object>
             </child>
           </object>

shell/pps-application.c
@@ -450,13 +450,7 @@ pps_application_startup (GApplication *gapplication)
           "win.select-all",             "<Ctrl>A", NULL,
           "win.close",                  "<Ctrl>W", NULL,
           "win.escape",                 "Escape", NULL,
-          "win.find-next",              "<Ctrl>G", "F3", NULL,
-          "win.find-previous",          "<Ctrl><Shift>G", "<Shift>F3", NULL,
           "win.select-page",            "<Ctrl>L", NULL,
-          "win.go-backwards",           "<Shift>Page_Up", NULL,
-          "win.go-forward",             "<Shift>Page_Down", NULL,
-          "win.go-back-history",        "<alt>P", "Back", NULL,
-          "win.go-forward-history",     "<alt>N", "Forward", NULL,
           "win.default-zoom",           "<Ctrl>0", "<Ctrl>KP_0", NULL,
           "win.caret-navigation",       "F7", NULL,
           "win.show-side-pane",         "F9", NULL,

shell/pps-window.ui
@@ -134,7 +134,7 @@
                     <child type="end">
                       <object class="GtkToggleButton" id="find_button">
                         <property name="icon-name">edit-find-symbolic</property>
-                        <property name="action-name">win.toggle-find</property>
+                        <property name="action-name">doc.toggle-find</property>
                         <signal name="notify::sensitive" handler="find_button_sensitive_changed" />
                       </object>
                     </child>
@@ -354,7 +354,43 @@
                     <child>
                       <object class='GtkShortcut'>
                         <property name='trigger'>&lt;Ctrl&gt;F|slash|KP_Divide</property>
-                        <property name='action'>action(win.find)</property>
+                        <property name='action'>action(doc.find)</property>
+                      </object>
+                    </child>
+                    <child>
+                      <object class='GtkShortcut'>
+                        <property name='trigger'>&lt;Ctrl&gt;G|F3</property>
+                        <property name='action'>action(doc.find-next)</property>
+                      </object>
+                    </child>
+                    <child>
+                      <object class='GtkShortcut'>
+                        <property name='trigger'>&lt;Ctrl&gt;&lt;Shift&gt;G|&lt;Shift&gt;F3</property>
+                        <property name='action'>action(doc.find-previous)</property>
+                      </object>
+                    </child>
+                    <child>
+                      <object class='GtkShortcut'>
+                        <property name='trigger'>&lt;Shift&gt;Page_Down</property>
+                        <property name='action'>action(doc.go-forward)</property>
+                      </object>
+                    </child>
+                    <child>
+                      <object class='GtkShortcut'>
+                        <property name='trigger'>&lt;Shift&gt;Page_Up</property>
+                        <property name='action'>action(doc.go-backwards)</property>
+                      </object>
+                    </child>
+                    <child>
+                      <object class='GtkShortcut'>
+                        <property name='trigger'>&lt;alt&gt;N|Forward</property>
+                        <property name='action'>action(doc.go-forward-history)</property>
+                      </object>
+                    </child>
+                    <child>
+                      <object class='GtkShortcut'>
+                        <property name='trigger'>&lt;alt&gt;P|Back</property>
+                        <property name='action'>action(doc.go-back-history)</property>
                       </object>
                     </child>
                     <child>

commit 663737ba9cb1274c80b4407176b6ade0c1bb186a

shell: window: properly deactivate document actions
To be used with gtk_widget_action_set_enabled an action
has to be part of the window's action group, and cannot
be installed differently.

Thus, we need to store the document action group
separately, and activate from that group directly.

Fixes: 4e8b39747471321d835db792a551973b63f02c8b ("shell: reorganize actions for PpsWindow")
Closes: https://gitlab.gnome.org/GNOME/Incubator/papers/-/issues/138

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

shell/pps-window.c
@@ -152,6 +152,8 @@ typedef struct {
 	PpsMetadata *metadata;
 	PpsBookmarks *bookmarks;
 
+	GSimpleActionGroup *document_action_group;
+
 	/* Has the document been modified? */
 	gboolean is_modified;
 	gulong   modified_handler_id;
@@ -317,6 +319,18 @@ pps_window_set_action_enabled (PpsWindow   *pps_window,
 }
 
 static void
+pps_window_set_document_action_enabled (PpsWindow   *pps_window,
+					const char *name,
+					gboolean    enabled)
+{
+	PpsWindowPrivate *priv = GET_PRIVATE (pps_window);
+	GAction *action;
+
+	action = g_action_map_lookup_action (G_ACTION_MAP (priv->document_action_group), name);
+	g_simple_action_set_enabled (G_SIMPLE_ACTION (action), enabled);
+}
+
+static void
 pps_window_update_actions_sensitivity (PpsWindow *pps_window)
 {
 	PpsWindowPrivate *priv = GET_PRIVATE (pps_window);
@@ -407,10 +421,10 @@ pps_window_update_actions_sensitivity (PpsWindow *pps_window)
         /* Edit menu */
 	pps_window_set_action_enabled (pps_window, "select-all", has_pages &&
 				      can_get_text && !start_view_mode);
-	gtk_widget_action_set_enabled (GTK_WIDGET (pps_window), "doc.find",
-				       can_find && !start_view_mode);
-	gtk_widget_action_set_enabled (GTK_WIDGET (pps_window), "doc.toggle-find",
-				       can_find && !start_view_mode);
+	pps_window_set_document_action_enabled (pps_window, "find",
+						can_find && !start_view_mode);
+	pps_window_set_document_action_enabled (pps_window, "toggle-find",
+						can_find && !start_view_mode);
 	pps_window_set_action_enabled (pps_window, "add-text-annotation",
 				       can_annotate && !start_view_mode);
 	pps_window_set_action_enabled (pps_window, "rotate-left", has_pages &&
@@ -494,14 +508,14 @@ pps_window_update_actions_sensitivity (PpsWindow *pps_window)
 		pps_window_set_action_enabled (pps_window, "select-page", FALSE);
 	}
 
-	gtk_widget_action_set_enabled (GTK_WIDGET (pps_window), "doc.go-back-history",
-				      !pps_history_is_frozen (priv->history) &&
-				      pps_history_can_go_back (priv->history) &&
-				      !start_view_mode);
-	gtk_widget_action_set_enabled (GTK_WIDGET (pps_window), "doc.go-forward-history",
-				      !pps_history_is_frozen (priv->history) &&
-				      pps_history_can_go_forward (priv->history) &&
-				      !start_view_mode);
+	pps_window_set_document_action_enabled (pps_window, "go-back-history",
+						!pps_history_is_frozen (priv->history) &&
+						pps_history_can_go_back (priv->history) &&
+						!start_view_mode);
+	pps_window_set_document_action_enabled (pps_window, "go-forward-history",
+						!pps_history_is_frozen (priv->history) &&
+						pps_history_can_go_forward (priv->history) &&
+						!start_view_mode);
 
 	pps_window_set_action_enabled (pps_window, "caret-navigation",
 				      has_pages &&
@@ -4858,8 +4872,8 @@ pps_window_show_find_bar (PpsWindow *pps_window)
 	gtk_widget_grab_focus (priv->search_box);
 	g_action_group_change_action_state (G_ACTION_GROUP (pps_window), "show-sidebar",
 						g_variant_new_boolean (TRUE));
-	gtk_widget_action_set_enabled (GTK_WIDGET (pps_window), "doc.find-next", TRUE);
-	gtk_widget_action_set_enabled (GTK_WIDGET (pps_window), "doc.find-previous", TRUE);
+	pps_window_set_document_action_enabled (pps_window, "find-next", TRUE);
+	pps_window_set_document_action_enabled (pps_window, "find-previous", TRUE);
 }
 
 static void
@@ -4880,8 +4894,8 @@ pps_window_close_find_bar (PpsWindow *pps_window)
 	gtk_search_bar_set_search_mode (GTK_SEARCH_BAR (priv->search_bar), FALSE);
 	gtk_widget_grab_focus (priv->view);
 
-	gtk_widget_action_set_enabled (GTK_WIDGET (pps_window), "doc.find-next", FALSE);
-	gtk_widget_action_set_enabled (GTK_WIDGET (pps_window), "doc.find-previous", FALSE);
+	pps_window_set_document_action_enabled (pps_window, "find-next", FALSE);
+	pps_window_set_document_action_enabled (pps_window, "find-previous", FALSE);
 
 	pps_history_thaw (priv->history);
 }
@@ -5167,10 +5181,10 @@ history_changed_cb (PpsHistory *history,
 {
 	PpsWindowPrivate *priv = GET_PRIVATE (window);
 
-	gtk_widget_action_set_enabled (GTK_WIDGET (window), "doc.go-back-history",
-				      pps_history_can_go_back (priv->history));
-	gtk_widget_action_set_enabled (GTK_WIDGET (window), "doc.go-forward-history",
-				      pps_history_can_go_forward (priv->history));
+	pps_window_set_document_action_enabled (window, "go-back-history",
+						pps_history_can_go_back (priv->history));
+	pps_window_set_document_action_enabled (window, "go-forward-history",
+						pps_history_can_go_forward (priv->history));
 }
 
 static void
@@ -5982,7 +5996,6 @@ pps_window_emit_doc_loaded (PpsWindow *window)
 static void
 pps_window_init (PpsWindow *pps_window)
 {
-	GSimpleActionGroup *group;
 	guint page_cache_mb;
 	gboolean allow_links_change_zoom;
 	PpsWindowPrivate *priv = GET_PRIVATE (pps_window);
@@ -6040,16 +6053,16 @@ pps_window_init (PpsWindow *pps_window)
 	g_action_map_add_action_entries (G_ACTION_MAP (pps_window),
 					 actions, G_N_ELEMENTS (actions),
 					 pps_window);
-	group = g_simple_action_group_new ();
-	g_action_map_add_action_entries (G_ACTION_MAP (group),
+	priv->document_action_group = g_simple_action_group_new ();
+	g_action_map_add_action_entries (G_ACTION_MAP (priv->document_action_group),
 					 doc_actions,
 					 G_N_ELEMENTS (doc_actions),
 					 pps_window);
 	gtk_widget_insert_action_group (GTK_WIDGET (pps_window),
-					"doc", G_ACTION_GROUP (group));
+					"doc", G_ACTION_GROUP (priv->document_action_group));
 	// These are only enabled once the search has started
-	gtk_widget_action_set_enabled (GTK_WIDGET (pps_window), "doc.find-next", FALSE);
-	gtk_widget_action_set_enabled (GTK_WIDGET (pps_window), "doc.find-previous", FALSE);
+	pps_window_set_document_action_enabled (pps_window, "find-next", FALSE);
+	pps_window_set_document_action_enabled (pps_window, "find-previous", FALSE);
 
 	page_cache_mb = g_settings_get_uint (priv->settings,
 					     GS_PAGE_CACHE_SIZE);