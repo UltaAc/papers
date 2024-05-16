UltaAc/papers

commit b057a91de2426aeb5533a7a97853cbc9a7190f7e

window: toggle search on Ctrl+F with no text selection
Usually, Ctrl+F toggles the search interface in an app,
in Papers, however, it is a bit more complicated, because
it supports to use it to search for the current selection.

Hence, here, the toggling behaviour is bound to the case
where there is no text selection, while if there is one,
the search bar stays visible and instead is updated with
a new search query.

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

shell/pps-window.c
@@ -3919,20 +3919,26 @@ pps_window_cmd_find (GSimpleAction *action,
 		    gpointer       user_data)
 {
 	PpsView *view;
-	gchar *selected_text = NULL;
 	PpsWindow *pps_window = user_data;
 	PpsWindowPrivate *priv = GET_PRIVATE (pps_window);
-	view = PPS_VIEW (priv->view);
+	g_autofree gchar *selected_text = NULL;
+	gboolean is_toggled;
 
+	view = PPS_VIEW (priv->view);
         selected_text = pps_view_get_selected_text (view);
 
-        if (selected_text != NULL) {
+        if (selected_text != NULL && g_strcmp0(selected_text, "") != 0) {
 		GtkSearchEntry *entry = pps_search_box_get_entry (PPS_SEARCH_BOX (priv->search_box));
 		gtk_editable_set_text (GTK_EDITABLE (entry), selected_text);
-		g_free (selected_text);
-	}
+		pps_window_show_find_bar (pps_window, TRUE);
+	} else {
+		is_toggled = g_variant_get_boolean (g_action_group_get_action_state (G_ACTION_GROUP (pps_window),
+										     "toggle-find"));
 
-	pps_window_show_find_bar (pps_window, TRUE);
+		g_action_group_change_action_state (G_ACTION_GROUP (pps_window),
+						    "toggle-find",
+						    g_variant_new_boolean (!is_toggled));
+        }
 }
 
 static void

commit 003f229efe52408d35788e9e3006b801d9331d5b

shell: window: set correct toggle find state
The toggle find state is used for the search toggle
button, and the toggle find state ensure that the view
also gets the search results highlighting.

Fixes: b057a91de2426aeb5533a7a97853cbc9a7190f7e ("window: toggle search on Ctrl+F with no text selection")
Closes: https://gitlab.gnome.org/GNOME/Incubator/papers/-/issues/139

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

shell/pps-window.c
@@ -3735,6 +3735,7 @@ pps_window_cmd_find (GSimpleAction *action,
 	PpsWindow *pps_window = user_data;
 	PpsWindowPrivate *priv = GET_PRIVATE (pps_window);
 	g_autofree gchar *selected_text = NULL;
+	GSimpleAction *toggle_find_action = G_SIMPLE_ACTION (g_action_map_lookup_action (G_ACTION_MAP (priv->document_action_group), "toggle-find"));
 
 	view = PPS_VIEW (priv->view);
         selected_text = pps_view_get_selected_text (view);
@@ -3743,11 +3744,11 @@ pps_window_cmd_find (GSimpleAction *action,
 		GtkSearchEntry *entry = pps_search_box_get_entry (PPS_SEARCH_BOX (priv->search_box));
 		gtk_editable_set_text (GTK_EDITABLE (entry), selected_text);
 		gtk_editable_select_region (GTK_EDITABLE (entry), 0, -1);
-		pps_window_show_find_bar (pps_window);
+		g_simple_action_set_state (toggle_find_action, g_variant_new_boolean (FALSE));
 		pps_window_find_restart (pps_window);
-	} else {
-		gtk_widget_activate_action (GTK_WIDGET (pps_window), "doc.toggle-find", NULL);
 	}
+
+	g_action_activate (G_ACTION (toggle_find_action), NULL);
 }
 
 static void