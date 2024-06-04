UltaAc/papers

commit 29b38c6f45285c8a66f0ab73c5fbbde7b21ec07b

shell: find-sidebar: move signal to search context and connect to start/clear
This also disconnects the window from the find sidebar
even more with regard to search logic. It helps changing
the search context runtime, and thus paves the way for a
tabbed view.

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

shell/pps-find-sidebar.c
@@ -30,6 +30,8 @@
 #include <string.h>
 
 typedef struct {
+	PpsSearchContext *context;
+
         GtkWidget *list_view;
         GtkSingleSelection *selection;
         GListStore *model;
@@ -45,13 +47,6 @@ enum {
 
         N_COLUMNS
 };
-
-enum {
-        RESULT_ACTIVATED,
-        N_SIGNALS
-};
-
-static guint signals[N_SIGNALS];
 
 G_DEFINE_TYPE_WITH_PRIVATE (PpsFindSidebar, pps_find_sidebar, GTK_TYPE_STACK)
 
@@ -81,14 +76,13 @@ selection_changed_cb (GtkSelectionModel *selection,
                       guint              n_items,
                       PpsFindSidebar    *sidebar)
 {
+        PpsFindSidebarPrivate *priv = GET_PRIVATE (sidebar);
         PpsSearchResult *selected_result = PPS_SEARCH_RESULT (gtk_single_selection_get_selected_item (GTK_SINGLE_SELECTION (selection)));
 
-        if (selected_result == NULL)
-                return;
+	g_return_if_fail (priv->context != NULL);
+	g_return_if_fail (selected_result != NULL);
 
-        g_signal_emit (sidebar, signals[RESULT_ACTIVATED], 0,
-		       pps_search_result_get_page (selected_result),
-		       pps_search_result_get_index (selected_result));
+	pps_search_context_select_result (priv->context, selected_result);
 }
 
 static void
@@ -166,16 +160,6 @@ pps_find_sidebar_class_init (PpsFindSidebarClass *find_sidebar_class)
         gtk_widget_class_bind_template_callback (GTK_WIDGET_CLASS (find_sidebar_class), factory_unbind_cb);
 
         gtk_widget_class_bind_template_callback (GTK_WIDGET_CLASS (find_sidebar_class), selection_changed_cb);
-
-        signals[RESULT_ACTIVATED] =
-                g_signal_new ("result-activated",
-                              G_TYPE_FROM_CLASS (g_object_class),
-                              G_SIGNAL_RUN_LAST,
-                              0, NULL, NULL,
-                              g_cclosure_marshal_generic,
-                              G_TYPE_NONE, 2,
-                              G_TYPE_INT,
-                              G_TYPE_INT);
 }
 
 static void
@@ -597,4 +581,27 @@ pps_find_sidebar_next (PpsFindSidebar *sidebar)
 
         pos = gtk_single_selection_get_selected (priv->selection) + 1;
         gtk_list_view_scroll_to (list_view, pos, GTK_LIST_SCROLL_SELECT, NULL);
+}
+
+void
+pps_find_sidebar_set_search_context (PpsFindSidebar   *sidebar,
+				     PpsSearchContext *context)
+{
+        PpsFindSidebarPrivate *priv = GET_PRIVATE (sidebar);
+
+	g_return_if_fail (PPS_IS_SEARCH_CONTEXT (context));
+
+	if (priv->context != NULL) {
+		g_signal_handlers_disconnect_by_func (priv->context, pps_find_sidebar_start, sidebar);
+		g_signal_handlers_disconnect_by_func (priv->context, pps_find_sidebar_clear, sidebar);
+	}
+
+	priv->context = context;
+
+	g_signal_connect_object (priv->context, "started",
+				 G_CALLBACK (pps_find_sidebar_start),
+				 sidebar, G_CONNECT_SWAPPED);
+	g_signal_connect_object (priv->context, "cleared",
+				 G_CALLBACK (pps_find_sidebar_clear),
+				 sidebar, G_CONNECT_SWAPPED);
 }

shell/pps-window.c
@@ -4744,10 +4744,10 @@ attachment_bar_save_attachment_cb (GtkWidget     *attachbar,
 }
 
 static void
-find_sidebar_result_activated_cb (PpsFindSidebar *find_sidebar,
-				  gint           page,
-				  gint           result,
-				  PpsWindow      *window)
+find_sidebar_result_activated_cb (PpsSearchContext *context,
+				  gint              page,
+				  gint              result,
+				  PpsWindow        *window)
 {
 	PpsWindowPrivate *priv = GET_PRIVATE (window);
 
@@ -4772,7 +4772,6 @@ search_started_cb (PpsSearchContext *search_context,
 		return;
 
 	pps_view_find_started (PPS_VIEW (priv->view), job);
-	pps_find_sidebar_start (PPS_FIND_SIDEBAR (priv->find_sidebar), job);
 }
 
 static void
@@ -4782,7 +4781,6 @@ search_cleared_cb (PpsSearchContext *search_context,
 	PpsWindowPrivate *priv = GET_PRIVATE (pps_window);
 
 	pps_window_update_actions_sensitivity (pps_window);
-	pps_find_sidebar_clear (PPS_FIND_SIDEBAR (priv->find_sidebar));
 
 	pps_view_find_cancel (PPS_VIEW (priv->view));
 	gtk_widget_queue_draw (GTK_WIDGET (priv->view));
@@ -6047,6 +6045,7 @@ pps_window_init (PpsWindow *pps_window)
 	priv->search_context = pps_search_context_new (priv->model);
 
 	pps_search_box_set_search_context (PPS_SEARCH_BOX (priv->search_box), priv->search_context);
+	pps_find_sidebar_set_search_context (PPS_FIND_SIDEBAR (priv->find_sidebar), priv->search_context);
 
 	g_signal_connect_object (priv->search_context, "cleared",
 				 G_CALLBACK (search_cleared_cb),
@@ -6112,7 +6111,6 @@ pps_window_class_init (PpsWindowClass *pps_window_class)
 	gtk_widget_class_bind_template_child_private (widget_class, PpsWindow, attachment_popup);
 
 	/* bind signal callback */
-	gtk_widget_class_bind_template_callback (widget_class, find_sidebar_result_activated_cb);
 	gtk_widget_class_bind_template_callback (widget_class, activate_link_cb);
 	gtk_widget_class_bind_template_callback (widget_class, window_maximized_changed);
 	gtk_widget_class_bind_template_callback (widget_class, window_size_changed_cb);

shell/pps-find-sidebar.h
@@ -24,6 +24,7 @@
 #include <gtk/gtk.h>
 
 #include "pps-jobs.h"
+#include "pps-search-context.h"
 
 G_BEGIN_DECLS
 
@@ -55,5 +56,7 @@ void       pps_find_sidebar_update   (PpsFindSidebar *find_sidebar);
 void       pps_find_sidebar_clear    (PpsFindSidebar *find_sidebar);
 void       pps_find_sidebar_previous (PpsFindSidebar *find_sidebar);
 void       pps_find_sidebar_next     (PpsFindSidebar *find_sidebar);
+void       pps_find_sidebar_set_search_context (PpsFindSidebar   *find_sidebar,
+				                PpsSearchContext *context);
 
 G_END_DECLS

shell/pps-search-context.c
@@ -39,6 +39,8 @@ enum {
 };
 
 enum {
+        RESULT_ACTIVATED,
+
         STARTED,
 	FINISHED,
         CLEARED,
@@ -245,6 +247,15 @@ pps_search_context_class_init (PpsSearchContextClass *klass)
 	g_object_class_install_properties (gobject_class, NUM_PROPERTIES, props);
 
         /* Signals */
+        signals[RESULT_ACTIVATED] =
+                g_signal_new ("result-activated",
+                              G_TYPE_FROM_CLASS (gobject_class),
+                              G_SIGNAL_RUN_LAST,
+                              0, NULL, NULL,
+                              g_cclosure_marshal_generic,
+                              G_TYPE_NONE, 2,
+                              G_TYPE_INT,
+                              G_TYPE_INT);
         signals[STARTED] =
                 g_signal_new ("started",
                               G_OBJECT_CLASS_TYPE (gobject_class),
@@ -326,4 +337,13 @@ void
 pps_search_context_restart (PpsSearchContext *context)
 {
         search_changed_cb (context);
+}
+
+void
+pps_search_context_select_result (PpsSearchContext *context,
+				  PpsSearchResult  *result)
+{
+	g_signal_emit (context, signals[RESULT_ACTIVATED], 0,
+		       pps_search_result_get_page (result),
+		       pps_search_result_get_index (result));
 }

shell/pps-search-context.h
@@ -25,6 +25,7 @@
 
 #include "pps-document-model.h"
 #include "pps-metadata.h"
+#include "pps-search-result.h"
 
 G_BEGIN_DECLS
 
@@ -48,5 +49,7 @@ PpsFindOptions    pps_search_context_get_options (PpsSearchContext *context);
 void              pps_search_context_set_options (PpsSearchContext *context,
 					          PpsFindOptions    options);
 void              pps_search_context_restart     (PpsSearchContext *context);
+void              pps_search_context_select_result (PpsSearchContext *context,
+						    PpsSearchResult  *result);
 
 G_END_DECLS

shell/pps-window.ui
@@ -272,7 +272,6 @@
                         </child>
                         <child>
                           <object class="PpsFindSidebar" id="find_sidebar">
-                            <signal name="result-activated" handler="find_sidebar_result_activated_cb" />
                           </object>
                         </child>
                       </object>

commit 7ef29788199cac2f59181b64d8fa061aaa27d405

shell: fix referencing search context
The find sidebar, the search box, and the window hold a
reference to the search context. It depends on it, and
does not perform NULL checks, instead it should reference
it properly by increasing the reference counter, and clear
the reference on disposal.

Fixes: 29b38c6f45285c8a66f0ab73c5fbbde7b21ec07b ("shell: find-sidebar: move signal to search context and connect to start/clear")

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

shell/pps-find-sidebar.c
@@ -117,11 +117,23 @@ pps_find_sidebar_grab_focus (GtkWidget *widget)
 }
 
 static void
+pps_find_sidebar_dispose (GObject *object)
+{
+        PpsFindSidebar *sidebar = PPS_FIND_SIDEBAR (object);
+        PpsFindSidebarPrivate *priv = GET_PRIVATE (sidebar);
+
+	g_clear_object (&priv->context);
+
+        G_OBJECT_CLASS (pps_find_sidebar_parent_class)->dispose (object);
+}
+
+static void
 pps_find_sidebar_class_init (PpsFindSidebarClass *find_sidebar_class)
 {
         GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (find_sidebar_class);
 
         widget_class->grab_focus = pps_find_sidebar_grab_focus;
+	G_OBJECT_CLASS (find_sidebar_class)->dispose = pps_find_sidebar_dispose;
 
         gtk_widget_class_set_template_from_resource (widget_class,
                                                      "/org/gnome/papers/ui/find-sidebar.ui");
@@ -271,7 +283,8 @@ pps_find_sidebar_set_search_context (PpsFindSidebar   *sidebar,
 		g_signal_handlers_disconnect_by_func (priv->context, pps_find_sidebar_clear, sidebar);
 	}
 
-	priv->context = context;
+	g_clear_object (&priv->context);
+	priv->context = g_object_ref (context);
 
 	gtk_single_selection_set_model (priv->selection, pps_search_context_get_result_model (priv->context));
 	g_signal_connect_object (priv->context, "started",

shell/pps-search-box.c
@@ -250,6 +250,7 @@ pps_search_box_set_search_context (PpsSearchBox     *box,
 		g_signal_handlers_disconnect_by_func (priv->context, find_job_finished_cb, box);
 	}
 
+	g_clear_object (&priv->context);
 	priv->context = g_object_ref (context);
 
 	g_signal_connect_object (priv->context, "started",

shell/pps-window.c
@@ -6040,7 +6040,7 @@ pps_window_init (PpsWindow *pps_window)
 				priv->find_sidebar, "visible",
 				G_BINDING_SYNC_CREATE);
 
-	priv->search_context = pps_search_context_new (priv->model);
+	priv->search_context = g_object_ref_sink (pps_search_context_new (priv->model));
 
 	pps_find_sidebar_set_search_context (PPS_FIND_SIDEBAR (priv->find_sidebar), priv->search_context);
 