176360b1b9cdeeaab998f37adb4fe116aeea2bec
Qiu Wenbo <qiuwenbo@gnome.org>
2024-08-21 10:00:54 +0800

    shell-rs: Remove skip_assert_initialized in papers-shell
    Signed-off-by: Qiu Wenbo <qiuwenbo@gnome.org>

--- shell-rs/papers-shell/src/lib.rs
+++ shell-rs/papers-shell/src/lib.rs
@@ -1,11 +1,6 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
 /// No-op.
-macro_rules! skip_assert_initialized {
-    () => {};
-}
-
-/// No-op.
 macro_rules! assert_initialized_main_thread {
     () => {};
 }
