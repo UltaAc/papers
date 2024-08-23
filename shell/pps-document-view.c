UltaAc/papers

commit e686a67c1ae8a6210951bd0888093a1d0293b177

libview+shell: move view search integration from window into view
This reduces the amount of signals the window listens to,
as well as the amount of things the window touches per
signal handler.

Link: https://gitlab.gnome.org/GNOME/Incubator/papers/-/issues/141

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

shell/pps-document-view.c
@@ -3071,43 +3071,6 @@ attachment_bar_menu_popup_cb (GtkWidget        *attachbar,
 	return TRUE;
 }
 
-static void
-find_sidebar_result_activated_cb (PpsSearchContext *context,
-				  gint              page,
-				  gint              result,
-				  PpsDocumentView        *window)
-{
-	PpsDocumentViewPrivate *priv = GET_PRIVATE (window);
-
-	pps_view_find_set_result (PPS_VIEW (priv->view), page, result);
-	sidebar_navigate_to_view (window);
-}
-
-static void
-search_started_cb (PpsSearchContext *search_context,
-		   PpsJobFind       *job,
-		   PpsDocumentView        *pps_doc_view)
-{
-	PpsDocumentViewPrivate *priv = GET_PRIVATE (pps_doc_view);
-
-	if (!priv->document || !PPS_IS_DOCUMENT_FIND (priv->document))
-		return;
-
-	pps_view_find_started (PPS_VIEW (priv->view), job);
-}
-
-static void
-search_cleared_cb (PpsSearchContext *search_context,
-		   PpsDocumentView        *pps_doc_view)
-{
-	PpsDocumentViewPrivate *priv = GET_PRIVATE (pps_doc_view);
-
-	pps_document_view_update_actions_sensitivity (pps_doc_view);
-
-	pps_view_find_cancel (PPS_VIEW (priv->view));
-	gtk_widget_queue_draw (GTK_WIDGET (priv->view));
-}
-
 void
 pps_document_view_handle_annot_popup (PpsDocumentView     *pps_doc_view,
 			      PpsAnnotation *annot)
@@ -4138,17 +4101,15 @@ pps_document_view_init (PpsDocumentView *pps_doc_view)
 	priv->search_context = g_object_ref_sink (pps_search_context_new (priv->model));
 
 	pps_find_sidebar_set_search_context (PPS_FIND_SIDEBAR (priv->find_sidebar), priv->search_context);
+	pps_view_set_search_context (PPS_VIEW (priv->view), priv->search_context);
 
 	g_signal_connect_object (priv->search_context, "cleared",
-				 G_CALLBACK (search_cleared_cb),
-				 pps_doc_view, G_CONNECT_DEFAULT);
-	g_signal_connect_object (priv->search_context, "started",
-				 G_CALLBACK (search_started_cb),
-				 pps_doc_view, G_CONNECT_DEFAULT);
-	g_signal_connect_object (priv->search_context, "result-activated",
-				 G_CALLBACK (find_sidebar_result_activated_cb),
+				 G_CALLBACK (pps_document_view_update_actions_sensitivity),
 				 pps_doc_view, G_CONNECT_DEFAULT);
 
+	g_signal_connect_object (priv->search_context, "result-activated",
+				 G_CALLBACK (sidebar_navigate_to_view),
+				 pps_doc_view, G_CONNECT_SWAPPED);
 	g_signal_connect_object (priv->sidebar, "navigated-to-view",
 				 G_CALLBACK (sidebar_navigate_to_view),
 				 pps_doc_view, G_CONNECT_SWAPPED);

libview/pps-view-private.h
@@ -99,6 +99,7 @@ typedef struct _PpsViewPrivate {
 	PpsDocument *document;
 
 	/* Find */
+	PpsSearchContext *search_context;
 	PpsJobFind *find_job;
 	GList **find_pages; /* Backwards compatibility. Contains PpsFindRectangles's elements per page */
 	gint find_page;     /* Page of active find result */

libview/pps-view.c
@@ -255,6 +255,8 @@ static void       jump_to_find_result                        (PpsView           
 static void       jump_to_find_page                          (PpsView             *view,
 							      PpsViewFindDirection direction,
 							      gint                shift);
+static void       pps_view_find_cancel                       (PpsView         *view);
+
 /*** Selection ***/
 static void       compute_selections                         (PpsView             *view,
 							      PpsSelectionStyle    style,
@@ -8658,7 +8660,7 @@ jump_to_find_page (PpsView *view, PpsViewFindDirection direction, gint shift)
 }
 
 static void
-find_job_finished_cb (PpsJobFind *job, PpsView *view)
+find_job_finished_cb (PpsView *view)
 {
 	jump_to_find_page (view, PPS_VIEW_FIND_NEXT, 0);
 	jump_to_find_result (view);
@@ -8677,14 +8679,7 @@ find_job_updated_cb (PpsJobFind *job, gint page, PpsView *view)
 		gtk_widget_queue_draw (GTK_WIDGET (view));
 }
 
-/**
- * pps_view_find_started:
- * @view:
- * @job:
- *
- * Since: 3.6
- */
-void
+static void
 pps_view_find_started (PpsView *view, PpsJobFind *job)
 {
 	PpsViewPrivate *priv = GET_PRIVATE (view);
@@ -8698,7 +8693,37 @@ pps_view_find_started (PpsView *view, PpsJobFind *job)
 	priv->find_result = 0;
 
 	g_signal_connect (job, "updated", G_CALLBACK (find_job_updated_cb), view);
-	g_signal_connect (job, "finished", G_CALLBACK (find_job_finished_cb), view);
+}
+
+void
+pps_view_set_search_context (PpsView          *view,
+			     PpsSearchContext *context)
+{
+        PpsViewPrivate *priv = GET_PRIVATE (view);
+
+	g_return_if_fail (PPS_IS_SEARCH_CONTEXT (context));
+
+	if (priv->search_context != NULL) {
+		g_signal_handlers_disconnect_by_func (priv->search_context, pps_view_find_started, view);
+		g_signal_handlers_disconnect_by_func (priv->search_context, pps_view_find_cancel, view);
+		g_signal_handlers_disconnect_by_func (priv->search_context, find_job_finished_cb, view);
+		g_signal_handlers_disconnect_by_func (priv->search_context, pps_view_find_set_result, view);
+	}
+
+	g_set_object (&priv->search_context, context);
+
+	g_signal_connect_object (priv->search_context, "started",
+				 G_CALLBACK (pps_view_find_started),
+				 view, G_CONNECT_SWAPPED);
+	g_signal_connect_object (priv->search_context, "cleared",
+				 G_CALLBACK (pps_view_find_cancel),
+				 view, G_CONNECT_SWAPPED);
+	g_signal_connect_object (priv->search_context, "finished",
+				 G_CALLBACK (find_job_finished_cb),
+				 view, G_CONNECT_SWAPPED);
+	g_signal_connect_object (priv->search_context, "result-activated",
+				 G_CALLBACK (pps_view_find_set_result),
+				 view, G_CONNECT_SWAPPED);
 }
 
 /**
@@ -8799,7 +8824,7 @@ pps_view_find_set_highlight_search (PpsView *view, gboolean value)
 	gtk_widget_queue_draw (GTK_WIDGET (view));
 }
 
-void
+static void
 pps_view_find_cancel (PpsView *view)
 {
 	PpsViewPrivate *priv = GET_PRIVATE (view);
@@ -8811,8 +8836,9 @@ pps_view_find_cancel (PpsView *view)
 		return;
 
 	g_signal_handlers_disconnect_by_func (priv->find_job, find_job_updated_cb, view);
-	g_signal_handlers_disconnect_by_func (priv->find_job, find_job_finished_cb, view);
 	g_clear_object (&priv->find_job);
+
+	gtk_widget_queue_draw (GTK_WIDGET (view));
 }
 
 /*** Selections ***/

libview/pps-view.h
@@ -28,6 +28,7 @@
 #include <papers-document.h>
 
 #include "context/pps-document-model.h"
+#include "context/pps-search-context.h"
 #include "pps-jobs.h"
 
 G_BEGIN_DECLS
@@ -80,8 +81,8 @@ void		pps_view_zoom_out	  (PpsView         *view);
 
 /* Find */
 PPS_PUBLIC
-void            pps_view_find_started              (PpsView         *view,
-						   PpsJobFind      *job);
+void            pps_view_set_search_context        (PpsView   *view,
+						    PpsSearchContext *context);
 PPS_PUBLIC
 void            pps_view_find_restart              (PpsView         *view,
                                                    gint            page);
@@ -96,8 +97,6 @@ void            pps_view_find_set_result           (PpsView         *view,
 PPS_PUBLIC
 void     	pps_view_find_set_highlight_search (PpsView         *view,
 						   gboolean        value);
-PPS_PUBLIC
-void            pps_view_find_cancel               (PpsView         *view);
 
 /* Navigation */
 PPS_PUBLIC

commit cb60f03003f7238962735fd855ff5e66db8142d0

shell: document-view: remove unnecessary signal action update
The signal handler should have been connected as
G_CONNECT_SWAPPED, as otherwise the signature mismatch
leads to a crash.

However, the signal handling can be removed altogether,
as the document actions do not depend on the search being
active or not – besides the find-prev/-next, that is, but
they are not handled in
pps_document_view_update_actions_sensitivity.

Fixes: e686a67c1ae8a6210951bd0888093a1d0293b177 ("libview+shell: move view search integration from window into view")
Closes: https://gitlab.gnome.org/GNOME/Incubator/papers/-/issues/229

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

shell/pps-document-view.c
@@ -4202,10 +4202,6 @@ pps_document_view_init (PpsDocumentView *pps_doc_view)
 	pps_find_sidebar_set_search_context (PPS_FIND_SIDEBAR (priv->find_sidebar), priv->search_context);
 	pps_view_set_search_context (PPS_VIEW (priv->view), priv->search_context);
 
-	g_signal_connect_object (priv->search_context, "cleared",
-				 G_CALLBACK (pps_document_view_update_actions_sensitivity),
-				 pps_doc_view, G_CONNECT_DEFAULT);
-
 	g_signal_connect_object (priv->search_context, "result-activated",
 				 G_CALLBACK (sidebar_navigate_to_view),
 				 pps_doc_view, G_CONNECT_SWAPPED);