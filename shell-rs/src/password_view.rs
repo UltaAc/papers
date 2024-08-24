UltaAc/papers

commit ed9f9d66ceb95becc300b9448afccb229ed86296

shell-rs: port PpsPasswordView to Rust

shell-rs/src/password_view.rs
@@ -1,0 +1,231 @@
+use crate::deps::*;
+
+use gio::PasswordSave;
+
+const CAN_SAVE_PASSWORD: bool = cfg!(feature = "with-keyring");
+
+const PASSWORD_PREFERENCE_NEVER: &str = "never";
+const PASSWORD_PREFERENCE_SESSION: &str = "for-session";
+const PASSWORD_PREFERENCE_PERMANENTLY: &str = "permanently";
+
+const RESPONSE_UNLOCK: &str = "unlock";
+const RESPONSE_CANCELLED: &str = "cancelled";
+
+mod imp {
+    use super::*;
+
+    #[derive(Debug, Clone)]
+    struct PasswordDialog {
+        main: adw::MessageDialog,
+        password_entry: gtk::PasswordEntry,
+    }
+
+    impl PasswordDialog {
+        fn new(password_view: &super::PpsPasswordView, error: bool) -> Self {
+            let builder = gtk::Builder::new();
+
+            let scope = gtk::BuilderRustScope::new();
+            PpsPasswordView::add_callbacks_to_scope(&scope);
+
+            builder.set_scope(Some(&scope));
+            builder.set_current_object(Some(password_view));
+
+            builder
+                .add_from_resource("/org/gnome/papers/ui/password-dialog.ui")
+                .unwrap();
+
+            if CAN_SAVE_PASSWORD {
+                builder
+                    .object::<gtk::Widget>("password_choice")
+                    .unwrap()
+                    .set_visible(true);
+            }
+
+            let password_entry: gtk::PasswordEntry = builder.object("password_entry").unwrap();
+
+            if error {
+                password_entry.add_css_class("error");
+
+                builder
+                    .object::<gtk::Label>("error_message")
+                    .unwrap()
+                    .set_visible(true);
+            }
+
+            PasswordDialog {
+                main: builder.object("dialog").unwrap(),
+                password_entry,
+            }
+        }
+    }
+
+    #[derive(Debug, Default, CompositeTemplate, Properties)]
+    #[properties(wrapper_type = super::PpsPasswordView)]
+    #[template(resource = "/org/gnome/papers/ui/password-view.ui")]
+    pub struct PpsPasswordView {
+        // FIXME: replace with a proper method once everything is full Rust
+        #[property(type = bool, set = Self::ask_password, name = "ask-password")]
+        #[allow(unused)]
+        active: (),
+        #[property(get, set)]
+        filename: RefCell<String>,
+        dialog: RefCell<Option<PasswordDialog>>,
+        #[template_child]
+        pub(super) action_group: TemplateChild<gio::SimpleActionGroup>,
+    }
+
+    #[glib::object_subclass]
+    impl ObjectSubclass for PpsPasswordView {
+        const NAME: &'static str = "PpsPasswordView";
+        type Type = super::PpsPasswordView;
+        type ParentType = adw::Bin;
+
+        fn class_init(klass: &mut Self::Class) {
+            klass.bind_template();
+            klass.bind_template_callbacks();
+        }
+
+        fn instance_init(obj: &InitializingObject<Self>) {
+            obj.init_template();
+        }
+    }
+
+    impl BinImpl for PpsPasswordView {}
+
+    impl WidgetImpl for PpsPasswordView {}
+
+    #[glib::derived_properties]
+    impl ObjectImpl for PpsPasswordView {
+        fn constructed(&self) {
+            let preferences_action = gio::SimpleAction::new_stateful(
+                "preference",
+                Some(glib::VariantTy::STRING),
+                &glib::Variant::from(PASSWORD_PREFERENCE_NEVER),
+            );
+
+            self.action_group.add_action(&preferences_action);
+        }
+
+        fn signals() -> &'static [Signal] {
+            static SIGNALS: OnceLock<Vec<Signal>> = OnceLock::new();
+
+            SIGNALS.get_or_init(|| {
+                vec![
+                    Signal::builder("unlock")
+                        .param_types([glib::Type::STRING, PasswordSave::static_type()])
+                        .run_last()
+                        .build(),
+                    Signal::builder("cancelled").run_last().build(),
+                ]
+            })
+        }
+    }
+
+    #[gtk::template_callbacks]
+    impl PpsPasswordView {
+        fn ask_password(&self, error: bool) {
+            if self.dialog.borrow().is_none() {
+                self.create_dialog(error);
+            }
+        }
+
+        #[template_callback]
+        fn open_unlock_dialog(&self) {
+            self.create_dialog(false);
+        }
+
+        fn create_dialog(&self, error: bool) {
+            let dialog = PasswordDialog::new(&self.obj(), error);
+
+            let window = self
+                .obj()
+                .root()
+                .and_then(|root| root.downcast::<gtk::Window>().ok());
+            dialog.main.set_transient_for(window.as_ref());
+
+            dialog.main.set_close_response(RESPONSE_CANCELLED);
+            dialog.main.set_default_response(Some(RESPONSE_UNLOCK));
+
+            dialog
+                .main
+                .insert_action_group("password", Some(&self.action_group.clone()));
+
+            let body = gettext!(
+                "The document “{}” is locked and requires a password before it can be opened",
+                self.filename.borrow()
+            );
+
+            dialog.main.set_body(&body);
+
+            let main = dialog.main.clone();
+
+            self.dialog.replace(Some(dialog));
+
+            main.choose(
+                gio::Cancellable::NONE,
+                glib::clone![@weak self as this => move |response| {
+                    this.handle_response(&response);
+                }],
+            );
+        }
+
+        #[template_callback]
+        fn update_password(&self, password_entry: gtk::PasswordEntry) {
+            let has_password = !password_entry.text().is_empty();
+
+            if let Some(dialog) = &*self.dialog.borrow() {
+                dialog
+                    .main
+                    .set_response_enabled(RESPONSE_UNLOCK, has_password);
+            }
+        }
+
+        fn handle_response(&self, response: &str) {
+            let dialog = self.dialog.borrow_mut().take().unwrap();
+
+            let preferences_action = self
+                .action_group
+                .lookup_action("preference")
+                .and_downcast::<gio::SimpleAction>()
+                .unwrap();
+
+            preferences_action.set_state(&glib::Variant::from(PASSWORD_PREFERENCE_NEVER));
+
+            match response {
+                RESPONSE_UNLOCK => {
+                    let password = dialog.password_entry.text();
+
+                    let preference = preferences_action
+                        .state()
+                        .filter(|_| CAN_SAVE_PASSWORD)
+                        .and_then(|value| value.get::<String>())
+                        .map(|pref_str| preference_choice_to_password_save(&pref_str[..]))
+                        .unwrap_or(PasswordSave::Never);
+
+                    preferences_action.set_state(&glib::Variant::from(PASSWORD_PREFERENCE_NEVER));
+
+                    self.obj()
+                        .emit_by_name::<()>("unlock", &[&password, &preference]);
+                }
+                RESPONSE_CANCELLED => {
+                    self.obj().emit_by_name::<()>("cancelled", &[]);
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    fn preference_choice_to_password_save(choice: &str) -> PasswordSave {
+        match choice {
+            PASSWORD_PREFERENCE_NEVER => PasswordSave::Never,
+            PASSWORD_PREFERENCE_SESSION => PasswordSave::ForSession,
+            PASSWORD_PREFERENCE_PERMANENTLY => PasswordSave::Permanently,
+            _ => unreachable!(),
+        }
+    }
+}
+
+glib::wrapper! {
+    pub struct PpsPasswordView(ObjectSubclass<imp::PpsPasswordView>)
+        @extends gtk::Widget, adw::Bin;
+}

shell-rs/Cargo.toml
@@ -10,6 +10,9 @@ rust-version = "1.70"
 [profile.release]
 lto = true
 
+[features]
+with-keyring = []
+
 [dependencies]
 env_logger = "0.10"
 gettext-rs = { version = "0.7", features = ["gettext-system"] }

shell-rs/src/main.rs
@@ -8,6 +8,7 @@ use config::GETTEXT_PACKAGE;
 use papers_shell::Application;
 
 mod deps;
+mod password_view;
 mod properties_fonts;
 mod properties_general;
 mod properties_license;
@@ -27,12 +28,13 @@ fn ensure_type() {
     let _ = gtk::init();
 
     // Hack: ensure type here so we don't need to add C interface
+    password_view::PpsPasswordView::ensure_type();
     properties_general::PpsPropertiesGeneral::ensure_type();
     properties_window::PpsPropertiesWindow::ensure_type();
     sidebar_attachments::PpsSidebarAttachments::ensure_type();
+    sidebar_layers::PpsSidebarLayers::ensure_type();
     sidebar_links::PpsSidebarLinks::ensure_type();
     sidebar_thumbnails::PpsSidebarThumbnails::ensure_type();
-    sidebar_layers::PpsSidebarLayers::ensure_type();
     sidebar_thumbnails::PpsSidebarThumbnails::ensure_type();
     stack_switcher::PpsStackSwitcher::ensure_type();
     zoom_action::PpsZoomAction::ensure_type();

shell-rs/src/meson.build
@@ -19,6 +19,10 @@ else
   rust_target = 'release'
 endif
 
+if enable_keyring
+  cargo_options += [ '--features', 'with-keyring' ]
+endif
+
 cargo_env = environment()
 # it seems that flatpak can't access network during install stage.
 cargo_env.set('CARGO_HOME', build_root / 'cargo-home')

shell/meson.build
@@ -9,7 +9,6 @@ papers_sources = files(
   'pps-keyring.c',
   'pps-message-area.c',
   'pps-metadata.c',
-  'pps-password-view.c',
   'pps-progress-message-area.c',
   'pps-utils.c',
   'pps-window.c',

shell/pps-keyring.c
@@ -39,16 +39,6 @@ static const SecretSchema doc_password_schema = {
 const SecretSchema *PPS_DOCUMENT_PASSWORD_SCHEMA = &doc_password_schema;
 #endif /* WITH_KEYRING */
 
-gboolean
-pps_keyring_is_available (void)
-{
-#ifdef WITH_KEYRING
-	return TRUE;
-#else
-	return FALSE;
-#endif
-}
-
 gchar *
 pps_keyring_lookup_password (const gchar *uri)
 {

shell/pps-keyring.h
@@ -25,7 +25,6 @@
 
 G_BEGIN_DECLS
 
-gboolean pps_keyring_is_available    (void);
 gchar   *pps_keyring_lookup_password (const gchar  *uri);
 gboolean pps_keyring_save_password   (const gchar  *uri,
 				     const gchar  *password,

shell/pps-password-view.c
@@ -1,357 +1,0 @@
-/* this file is part of papers, a gnome document viewer
- *
- *  Copyright (C) 2018 Germán Poo-Caamaño <gpoo@gnome.org>
- *  Copyright (C) 2008 Carlos Garcia Campos <carlosgc@gnome.org>
- *  Copyright (C) 2005 Red Hat, Inc
- *
- * Papers is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Papers is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- */
-
-
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
-
-#include <glib/gi18n.h>
-#include <gtk/gtk.h>
-#include <gio/gio.h>
-
-#include "pps-keyring.h"
-#include "pps-password-view.h"
-
-/* Define a maximum width in case there is a file with a very long name */
-#define MAX_WIDHT_LABEL 64
-/* Define a maximum width for password entry */
-#define MAX_WIDHT_PASSWORD_ENTRY 32
-
-enum {
-	UNLOCK,
-	CANCELLED,
-	LAST_SIGNAL
-};
-
-enum {
-	PROP_FILENAME = 1,
-	PROP_ASK_PASSWORD,
-	N_PROPERTIES,
-};
-
-static GParamSpec *obj_properties[N_PROPERTIES] = { NULL, };
-
-typedef struct {
-	GtkWidget    *password_entry;
-
-	gchar        *password;
-	GPasswordSave password_save;
-
-	char         *filename;
-} PpsPasswordViewPrivate;
-
-static guint password_view_signals [LAST_SIGNAL] = { 0 };
-
-#define GET_PRIVATE(o) pps_password_view_get_instance_private (o)
-
-G_DEFINE_TYPE_WITH_PRIVATE (PpsPasswordView, pps_password_view, ADW_TYPE_BIN)
-
-static void pps_password_view_clicked_cb (GtkWidget      *button,
-					 PpsPasswordView *password_view);
-static void
-pps_password_view_set_property (GObject      *object,
-					                      guint         prop_id,
-					                      const GValue *value,
-					                      GParamSpec   *pspec);
-static void
-pps_password_view_set_filename (PpsPasswordView *password_view,
-                                const char     *filename);
-static void pps_password_view_ask_password  (PpsPasswordView *password_view, gboolean error);
-
-static void
-pps_password_view_finalize (GObject *object)
-{
-	PpsPasswordView *password_view = PPS_PASSWORD_VIEW (object);
-	PpsPasswordViewPrivate *priv = GET_PRIVATE (password_view);
-
-	g_clear_pointer (&priv->password, g_free);
-	g_clear_pointer (&priv->filename, g_free);
-
-	G_OBJECT_CLASS (pps_password_view_parent_class)->finalize (object);
-}
-
-static void
-pps_password_view_class_init (PpsPasswordViewClass *class)
-{
-	GObjectClass *g_object_class;
-	GtkWidgetClass *widget_class;
-
-	g_object_class = G_OBJECT_CLASS (class);
-	widget_class = GTK_WIDGET_CLASS (class);
-
-	g_object_class->set_property = pps_password_view_set_property;
-
-	obj_properties[PROP_FILENAME] =
-		g_param_spec_string ("filename",
-		                     "Filename",
-		                     "Name of the document to unlock.",
-		                     NULL,
-		                     G_PARAM_WRITABLE);
-
-	obj_properties[PROP_ASK_PASSWORD] =
-		g_param_spec_boolean ("ask-password",
-		                      "Ask Password",
-		                      "Triggers a password prompt, optionally erroneous if set",
-		                      FALSE,
-		                      G_PARAM_WRITABLE);
-
-	g_object_class_install_properties (g_object_class, N_PROPERTIES, obj_properties);
-
-	password_view_signals[UNLOCK] =
-		g_signal_new ("unlock",
-			      G_TYPE_FROM_CLASS (g_object_class),
-			      G_SIGNAL_RUN_LAST | G_SIGNAL_ACTION,
-			      0,
-			      NULL, NULL,
-			      NULL,
-			      G_TYPE_NONE,
-			      2, G_TYPE_STRING, G_TYPE_PASSWORD_SAVE);
-
-	password_view_signals[CANCELLED] =
-		g_signal_new ("cancelled",
-			      G_TYPE_FROM_CLASS (g_object_class),
-			      G_SIGNAL_RUN_LAST | G_SIGNAL_ACTION,
-			      0,
-			      NULL, NULL,
-			      g_cclosure_marshal_VOID__VOID,
-			      G_TYPE_NONE, 0);
-
-	gtk_widget_class_set_template_from_resource (widget_class,
-				"/org/gnome/papers/ui/password-view.ui");
-	gtk_widget_class_bind_template_callback (widget_class,
-						 pps_password_view_clicked_cb);
-
-	g_object_class->finalize = pps_password_view_finalize;
-}
-
-static void
-pps_password_view_set_property (GObject      *object,
-					                      guint         prop_id,
-					                      const GValue *value,
-					                      GParamSpec   *pspec)
-{
-	PpsPasswordView *view = PPS_PASSWORD_VIEW (object);
-
-	switch (prop_id) {
-		case PROP_FILENAME:
-			pps_password_view_set_filename (view, g_value_get_string(value));
-			break;
-
-		case PROP_ASK_PASSWORD:
-			pps_password_view_ask_password(view, g_value_get_boolean(value));
-			break;
-
-		default:
-			G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
-	}
-}
-
-static void
-pps_password_view_clicked_cb (GtkWidget      *button,
-			     PpsPasswordView *password_view)
-{
-	pps_password_view_ask_password (password_view, FALSE);
-}
-
-static void
-pps_password_view_init (PpsPasswordView *password_view)
-{
-	PpsPasswordViewPrivate *priv = GET_PRIVATE (password_view);
-
-	priv->password_save = G_PASSWORD_SAVE_NEVER;
-
-	gtk_widget_init_template (GTK_WIDGET (password_view));
-}
-
-static void
-pps_password_view_set_filename (PpsPasswordView *password_view,
-                                const char     *filename)
-{
-	PpsPasswordViewPrivate *priv = GET_PRIVATE (password_view);
-
-	g_return_if_fail (PPS_IS_PASSWORD_VIEW (password_view));
-	g_return_if_fail (filename != NULL);
-
-	if (g_strcmp0 (priv->filename, filename) == 0)
-		return;
-
-	g_free (priv->filename);
-	priv->filename = g_strdup (filename);
-}
-
-static void
-pps_password_dialog_got_response (AdwMessageDialog *dialog,
-                                 GAsyncResult     *result,
-                                 PpsPasswordView   *password_view)
-{
-	PpsPasswordViewPrivate *priv = GET_PRIVATE (password_view);
-	const char *response = adw_message_dialog_choose_finish (dialog, result);
-
-	gtk_widget_set_sensitive (GTK_WIDGET (password_view), TRUE);
-
-	if (g_strcmp0 (response, "unlock") == 0) {
-		priv->password =
-			g_strdup (gtk_editable_get_text (GTK_EDITABLE (priv->password_entry)));
-
-		g_signal_emit (password_view, password_view_signals[UNLOCK], 0, g_strdup (priv->password), priv->password_save);
-	} else {
-		g_signal_emit (password_view, password_view_signals[CANCELLED], 0);
-	}
-}
-
-static void
-pps_password_dialog_remember_button_toggled (GtkCheckButton *button,
-					    PpsPasswordView  *password_view)
-{
-	PpsPasswordViewPrivate *priv = GET_PRIVATE (password_view);
-
-	if (gtk_check_button_get_active (button)) {
-		gpointer data;
-
-		data = g_object_get_data (G_OBJECT (button), "password-save");
-		priv->password_save = GPOINTER_TO_INT (data);
-	}
-}
-
-static void
-pps_password_dialog_entry_changed_cb (GtkEditable      *editable,
-                                     AdwMessageDialog *dialog)
-{
-	const char *text;
-
-	text = gtk_editable_get_text (GTK_EDITABLE (editable));
-
-	adw_message_dialog_set_response_enabled (dialog, "unlock",
-						 (text != NULL && *text != '\0'));
-}
-
-static void
-pps_password_view_ask_password (PpsPasswordView *password_view, gboolean error)
-{
-	AdwMessageDialog *dialog;
-	GtkWidget *message_area;
-	GtkWidget *password_entry;
-	gchar     *text;
-	PpsPasswordViewPrivate *priv = GET_PRIVATE (password_view);
-	GtkWindow *parent_window;
-
-        text = g_markup_printf_escaped (_("The document “%s” is locked and requires a password before it can be opened"),
-                                        priv->filename);
-
-	parent_window = GTK_WINDOW (gtk_widget_get_root (GTK_WIDGET (password_view)));
-
-	dialog = ADW_MESSAGE_DIALOG (adw_message_dialog_new (parent_window,
-							     _("Password Required"),
-							     NULL));
-	adw_message_dialog_set_body_use_markup (dialog, true);
-	adw_message_dialog_set_body (dialog, text);
-	g_free (text);
-
-	adw_message_dialog_add_responses (dialog,
-					  "cancel", _("_Cancel"),
-					  "unlock", _("_Unlock"),
-					  NULL);
-	adw_message_dialog_set_close_response (dialog, "cancel");
-	adw_message_dialog_set_default_response (dialog, "unlock");
-	adw_message_dialog_set_response_enabled (dialog, "unlock", FALSE);
-
-	message_area = gtk_box_new (GTK_ORIENTATION_VERTICAL, 12);
-	adw_message_dialog_set_extra_child (dialog, message_area);
-
-	password_entry = gtk_password_entry_new ();
-	gtk_password_entry_set_show_peek_icon (GTK_PASSWORD_ENTRY (password_entry), TRUE);
-	g_object_set (G_OBJECT (password_entry),
-		      "placeholder-text", _("Password"),
-		      NULL);
-	g_object_set (G_OBJECT (password_entry), "activates-default", TRUE, NULL);
-	g_object_set (G_OBJECT (password_entry), "width-chars", 32, NULL);
-	g_signal_connect (password_entry, "changed",
-			  G_CALLBACK (pps_password_dialog_entry_changed_cb),
-			  dialog);
-
-	gtk_box_append (GTK_BOX (message_area), password_entry);
-
-	if (error) {
-		GtkWidget* error_message = gtk_label_new (_("Invalid password"));
-
-		gtk_box_append (GTK_BOX (message_area), error_message);
-
-		gtk_widget_add_css_class (error_message, "error");
-		gtk_widget_add_css_class (password_entry, "error");
-	}
-
-	priv->password_entry = password_entry;
-
-	if (pps_keyring_is_available ()) {
-		GtkWidget  *choice;
-		GtkWidget  *remember_box;
-		GtkWidget  *group;
-
-		remember_box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 6);
-		gtk_box_append (GTK_BOX (message_area), remember_box);
-		gtk_widget_set_halign (remember_box, GTK_ALIGN_CENTER);
-
-		choice = gtk_check_button_new_with_mnemonic (_("Forget password _immediately"));
-		gtk_check_button_set_active (GTK_CHECK_BUTTON (choice),
-					      priv->password_save == G_PASSWORD_SAVE_NEVER);
-		g_object_set_data (G_OBJECT (choice), "password-save",
-				   GINT_TO_POINTER (G_PASSWORD_SAVE_NEVER));
-		g_signal_connect (choice, "toggled",
-				  G_CALLBACK (pps_password_dialog_remember_button_toggled),
-				  password_view);
-		gtk_box_append (GTK_BOX (remember_box), choice);
-
-		group = choice;
-		choice = gtk_check_button_new_with_mnemonic (_("Remember password until you _log out"));
-		gtk_check_button_set_group (GTK_CHECK_BUTTON (choice), GTK_CHECK_BUTTON (group));
-		gtk_check_button_set_active (GTK_CHECK_BUTTON (choice),
-					      priv->password_save == G_PASSWORD_SAVE_FOR_SESSION);
-		g_object_set_data (G_OBJECT (choice), "password-save",
-				   GINT_TO_POINTER (G_PASSWORD_SAVE_FOR_SESSION));
-		g_signal_connect (choice, "toggled",
-				  G_CALLBACK (pps_password_dialog_remember_button_toggled),
-				  password_view);
-		gtk_box_append (GTK_BOX (remember_box), choice);
-
-		group = choice;
-		choice = gtk_check_button_new_with_mnemonic (_("Remember _forever"));
-		gtk_check_button_set_group (GTK_CHECK_BUTTON (choice), GTK_CHECK_BUTTON (group));
-		gtk_check_button_set_active (GTK_CHECK_BUTTON (choice),
-					      priv->password_save == G_PASSWORD_SAVE_PERMANENTLY);
-		g_object_set_data (G_OBJECT (choice), "password-save",
-				   GINT_TO_POINTER (G_PASSWORD_SAVE_PERMANENTLY));
-		g_signal_connect (choice, "toggled",
-				  G_CALLBACK (pps_password_dialog_remember_button_toggled),
-				  password_view);
-		gtk_box_append (GTK_BOX (remember_box), choice);
-	}
-
-	adw_message_dialog_choose (dialog, NULL,
-				   (GAsyncReadyCallback) pps_password_dialog_got_response,
-				   password_view);
-}
-
-PpsPasswordView *
-pps_password_view_new (void)
-{
-	return g_object_new (PPS_TYPE_PASSWORD_VIEW, NULL);
-}

commit d98d4202de10332e372fc7bd3492f9fcfeeb7fb1

shell-rs: Don't reset password preference to never when unlocking document
We can't save the new password when unlocking document, regardless of
whether you choose to remember it for the session or forever.

Fixes: ed9f9d66ceb9 ("shell-rs: port PpsPasswordView to Rust")

shell-rs/src/password_view.rs
@@ -195,8 +195,6 @@ mod imp {
                 .and_downcast::<gio::SimpleAction>()
                 .unwrap();
 
-            preferences_action.set_state(&glib::Variant::from(PASSWORD_PREFERENCE_NEVER));
-
             match response {
                 RESPONSE_UNLOCK => {
                     let password = dialog.password_entry.text();