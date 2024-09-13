0f1140e36a8268f7b89a1a76c348b420d8b88ad1
Balló György <ballogyor@gmail.com>
2024-09-13 17:56:50 +0000

    previewer: Set application ID
    This is needed for Wayland compositors to match the window with the .desktop file.

--- previewer/pps-previewer.c
+++ previewer/pps-previewer.c
@@ -273,7 +273,7 @@ main (gint argc, gchar **argv)
 	g_set_application_name (_("GNOME Document Previewer"));
 	gtk_window_set_default_icon_name (APP_ID);
 
-        application = adw_application_new (NULL, G_APPLICATION_NON_UNIQUE);
+        application = adw_application_new (APP_ID "-previewer", G_APPLICATION_NON_UNIQUE);
 	g_application_set_resource_base_path (G_APPLICATION (application),
 			"/org/gnome/papers/previewer");
         g_signal_connect (application, "startup", G_CALLBACK (startup_cb), NULL);
