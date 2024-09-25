UltaAc/papers

commit fec45cabfe6aae0e1be978fa3f38d12d6356a674

libview: Switch to use private class member for PpsJob
We add a new interface pps_job_reset to let the subclass to reset the
finished state.

Signed-off-by: Qiu Wenbo <qiuwenbo@gnome.org>

libview/pps-print-operation.c
@@ -2530,7 +2530,7 @@ pps_print_operation_print_draw_page (PpsPrintOperationPrint *print,
                 g_signal_connect (G_OBJECT (print->job_print), "cancelled",
                                   G_CALLBACK (print_job_cancelled),
                                   (gpointer)print);
-	} else if (g_cancellable_is_cancelled (print->job_print->cancellable)) {
+	} else if (pps_job_get_cancellable (print->job_print)) {
                 gtk_print_operation_cancel (print->op);
                 pps_job_print_set_cairo (PPS_JOB_PRINT (print->job_print), NULL);
                 return;

libview/pps-job-scheduler.c
@@ -70,7 +70,7 @@ pps_job_thread (PpsSchedulerJob *scheduler_job, gpointer data)
 
 	g_debug ("running thread for job: %s", PPS_GET_TYPE_NAME (job));
 
-	if (!g_cancellable_is_cancelled (job->cancellable))
+	if (!g_cancellable_is_cancelled (pps_job_get_cancellable (job)))
 		pps_job_run (job);
 
 	pps_scheduler_job_destroy (scheduler_job);

libview/pps-job.c
@@ -41,24 +41,42 @@ enum {
 
 static guint job_signals[LAST_SIGNAL] = { 0 };
 
-G_DEFINE_ABSTRACT_TYPE (PpsJob, pps_job, G_TYPE_OBJECT)
+typedef struct _PpsJobPrivate {
+	PpsDocument *document;
+
+	guint cancelled : 1;
+	guint finished : 1;
+	guint failed : 1;
+
+	GError *error;
+	GCancellable *cancellable;
+
+	guint idle_finished_id;
+	guint idle_cancelled_id;
+} PpsJobPrivate;
+
+#define GET_PRIVATE(o) pps_job_get_instance_private (o)
+
+G_DEFINE_ABSTRACT_TYPE_WITH_PRIVATE (PpsJob, pps_job, G_TYPE_OBJECT)
 
 static void
 pps_job_init (PpsJob *job)
 {
-	job->cancellable = g_cancellable_new ();
+	PpsJobPrivate *priv = GET_PRIVATE (job);
+	priv->cancellable = g_cancellable_new ();
 }
 
 static void
 pps_job_dispose (GObject *object)
 {
 	PpsJob *job = PPS_JOB (object);
+	PpsJobPrivate *priv = GET_PRIVATE (job);
 
 	g_debug ("disposing %s (%p)", PPS_GET_TYPE_NAME(job), job);
 
-	g_clear_object (&job->document);
-	g_clear_object (&job->cancellable);
-	g_clear_error (&job->error);
+	g_clear_object (&priv->document);
+	g_clear_object (&priv->cancellable);
+	g_clear_error (&priv->error);
 
 	(* G_OBJECT_CLASS (pps_job_parent_class)->dispose) (object);
 }
@@ -70,10 +88,11 @@ pps_job_set_property (GObject      *object,
 		     GParamSpec   *pspec)
 {
 	PpsJob *job = PPS_JOB (object);
+	PpsJobPrivate *priv = GET_PRIVATE (job);
 
 	switch (prop_id) {
 	case PROP_DOCUMENT:
-		job->document = g_value_dup_object (value);
+		priv->document = g_value_dup_object (value);
 		break;
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
@@ -119,11 +138,13 @@ pps_job_class_init (PpsJobClass *klass)
 static gboolean
 emit_finished (PpsJob *job)
 {
+	PpsJobPrivate *priv = GET_PRIVATE (job);
+
 	g_debug ("emit finished for %s (%p)", PPS_GET_TYPE_NAME (job), job);
 
-	job->idle_finished_id = 0;
+	priv->idle_finished_id = 0;
 
-	if (job->cancelled)
+	if (priv->cancelled)
 		g_debug ("%s (%p) job was cancelled, do not emit finished", PPS_GET_TYPE_NAME (job), job);
 	else
 		g_signal_emit (job, job_signals[FINISHED], 0);
@@ -134,14 +155,16 @@ emit_finished (PpsJob *job)
 static void
 pps_job_emit_finished (PpsJob *job)
 {
-	if (g_cancellable_is_cancelled (job->cancellable)) {
+	PpsJobPrivate *priv = GET_PRIVATE (job);
+
+	if (g_cancellable_is_cancelled (priv->cancellable)) {
 		g_debug ("%s (%p) job was cancelled, returning", PPS_GET_TYPE_NAME (job), job);
 		return;
 	}
 
-	job->finished = TRUE;
+	priv->finished = TRUE;
 
-	job->idle_finished_id =
+	priv->idle_finished_id =
 		g_idle_add_full (G_PRIORITY_DEFAULT_IDLE,
 				 (GSourceFunc)emit_finished,
 				 g_object_ref (job),
@@ -163,16 +186,18 @@ pps_job_run (PpsJob *job)
 void
 pps_job_cancel (PpsJob *job)
 {
-	if (job->cancelled)
+	PpsJobPrivate *priv = GET_PRIVATE (job);
+
+	if (priv->cancelled)
 		return;
 
 	g_debug ("job %s (%p) cancelled", PPS_GET_TYPE_NAME (job), job);
 
 	/* This should never be called from a thread */
-	job->cancelled = TRUE;
-	g_cancellable_cancel (job->cancellable);
+	priv->cancelled = TRUE;
+	g_cancellable_cancel (priv->cancellable);
 
-        if (job->finished && job->idle_finished_id == 0)
+        if (priv->finished && priv->idle_finished_id == 0)
                 return;
 
 	g_signal_emit (job, job_signals[CANCELLED], 0);
@@ -185,21 +210,22 @@ pps_job_failed (PpsJob       *job,
 	       const gchar *format,
 	       ...)
 {
+	PpsJobPrivate *priv = GET_PRIVATE (job);
 	va_list args;
 	gchar  *message;
 
-	if (job->failed || job->finished)
+	if (priv->failed || priv->finished)
 		return;
 
 	g_debug ("job %s (%p) failed", PPS_GET_TYPE_NAME (job), job);
 
-	job->failed = TRUE;
+	priv->failed = TRUE;
 
 	va_start (args, format);
 	message = g_strdup_vprintf (format, args);
 	va_end (args);
 
-	job->error = g_error_new_literal (domain, code, message);
+	priv->error = g_error_new_literal (domain, code, message);
 	g_free (message);
 
 	pps_job_emit_finished (job);
@@ -214,13 +240,15 @@ void
 pps_job_failed_from_error (PpsJob  *job,
 			  GError *error)
 {
-	if (job->failed || job->finished)
+	PpsJobPrivate *priv = GET_PRIVATE (job);
+
+	if (priv->failed || priv->finished)
 		return;
 
 	g_debug ("job %s (%p) failed", PPS_GET_TYPE_NAME (job), job);
 
-	job->failed = TRUE;
-	job->error = g_error_copy (error);
+	priv->failed = TRUE;
+	priv->error = g_error_copy (error);
 
 	pps_job_emit_finished (job);
 }
@@ -228,19 +256,23 @@ pps_job_failed_from_error (PpsJob  *job,
 void
 pps_job_succeeded (PpsJob *job)
 {
-	if (job->finished)
+	PpsJobPrivate *priv = GET_PRIVATE (job);
+
+	if (priv->finished)
 		return;
 
 	g_debug ("job %s (%p) succeeded", PPS_GET_TYPE_NAME (job), job);
 
-	job->failed = FALSE;
+	priv->failed = FALSE;
 	pps_job_emit_finished (job);
 }
 
 gboolean
 pps_job_is_finished (PpsJob *job)
 {
-	return job->finished;
+	PpsJobPrivate *priv = GET_PRIVATE (job);
+
+	return priv->finished;
 }
 
 /**
@@ -253,12 +285,14 @@ pps_job_is_finished (PpsJob *job)
 gboolean
 pps_job_is_succeeded (PpsJob   *job, GError **error)
 {
+	PpsJobPrivate *priv = GET_PRIVATE (job);
+
 	g_return_val_if_fail (job != NULL, FALSE);
 
-	if (job->failed && error)
-		*error = g_error_copy (job->error);
+	if (priv->failed && error)
+		*error = g_error_copy (priv->error);
 
-	return !job->failed;
+	return !priv->failed;
 }
 
 /**
@@ -270,7 +304,35 @@ pps_job_is_succeeded (PpsJob   *job, GError **error)
 PpsDocument *
 pps_job_get_document (PpsJob	 *job)
 {
+	PpsJobPrivate *priv = GET_PRIVATE (job);
+
 	g_return_val_if_fail (PPS_IS_JOB (job), NULL);
 
-	return job->document;
+	return priv->document;
+}
+
+/**
+ * pps_job_get_cancellable:
+ * @job: an #PpsJob
+ *
+ * Returns: (transfer none): The #GCancellable of this job.
+ */
+GCancellable *
+pps_job_get_cancellable (PpsJob	 *job)
+{
+	PpsJobPrivate *priv = GET_PRIVATE (job);
+
+	g_return_val_if_fail (PPS_IS_JOB (job), NULL);
+
+	return priv->cancellable;
+}
+
+void
+pps_job_reset (PpsJob *job)
+{
+	PpsJobPrivate *priv = GET_PRIVATE (job);
+
+	priv->failed = FALSE;
+	priv->finished = FALSE;
+	g_clear_error (&priv->error);
 }

libview/pps-job.h
@@ -33,32 +33,10 @@
 
 G_BEGIN_DECLS
 
-typedef struct _PpsJob PpsJob;
-typedef struct _PpsJobClass PpsJobClass;
-
 #define PPS_TYPE_JOB            (pps_job_get_type())
-#define PPS_JOB(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), PPS_TYPE_JOB, PpsJob))
-#define PPS_IS_JOB(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), PPS_TYPE_JOB))
-#define PPS_JOB_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), PPS_TYPE_JOB, PpsJobClass))
-#define PPS_IS_JOB_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), PPS_TYPE_JOB))
-#define PPS_JOB_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), PPS_TYPE_JOB, PpsJobClass))
 
-struct _PpsJob
-{
-	GObject parent;
-
-	PpsDocument *document;
-
-	guint cancelled : 1;
-	guint finished : 1;
-	guint failed : 1;
-
-	GError *error;
-	GCancellable *cancellable;
-
-	guint idle_finished_id;
-	guint idle_cancelled_id;
-};
+PPS_PUBLIC
+G_DECLARE_DERIVABLE_TYPE (PpsJob, pps_job, PPS, JOB, GObject)
 
 struct _PpsJobClass
 {
@@ -71,8 +49,7 @@ struct _PpsJobClass
 	void     (* finished)   (PpsJob *job);
 };
 
-PPS_PUBLIC
-GType           pps_job_get_type           (void) G_GNUC_CONST;
+
 PPS_PUBLIC
 gboolean        pps_job_run                (PpsJob          *job);
 PPS_PUBLIC
@@ -91,9 +68,14 @@ void            pps_job_succeeded          (PpsJob          *job);
 PPS_PUBLIC
 gboolean        pps_job_is_finished        (PpsJob          *job);
 PPS_PUBLIC
-gboolean        pps_job_is_succeeded          (PpsJob          *job,
+gboolean        pps_job_is_succeeded       (PpsJob          *job,
 					    GError         **error);
 PPS_PUBLIC
-PpsDocument     *pps_job_get_document	  (PpsJob	  *job);
+PpsDocument     *pps_job_get_document	   (PpsJob	  *job);
+PPS_PUBLIC
+GCancellable    *pps_job_get_cancellable   (PpsJob	  *job);
+
+PPS_PUBLIC
+void		pps_job_reset		   (PpsJob *job);
 
 G_END_DECLS

libview/pps-jobs.c
@@ -112,7 +112,7 @@ fill_page_labels (GListModel *model, PpsJob *job)
 		if (!link)
 			continue;;
 
-		document_links = PPS_DOCUMENT_LINKS (job->document);
+		document_links = PPS_DOCUMENT_LINKS (pps_job_get_document (job));
 		page_label = pps_document_links_get_link_page_label (document_links, link);
 		if (!page_label)
 			continue;
@@ -137,7 +137,7 @@ pps_job_links_run (PpsJob *job)
 	g_debug ("running links job");
 
 	pps_document_doc_mutex_lock ();
-	job_links->model = pps_document_links_get_links_model (PPS_DOCUMENT_LINKS (job->document));
+	job_links->model = pps_document_links_get_links_model (PPS_DOCUMENT_LINKS (pps_job_get_document (job)));
 	pps_document_doc_mutex_unlock ();
 
 	fill_page_labels (job_links->model, job);
@@ -212,7 +212,7 @@ pps_job_attachments_run (PpsJob *job)
 
 	pps_document_doc_mutex_lock ();
 	job_attachments->attachments =
-		pps_document_attachments_get_attachments (PPS_DOCUMENT_ATTACHMENTS (job->document));
+		pps_document_attachments_get_attachments (PPS_DOCUMENT_ATTACHMENTS (pps_job_get_document (job)));
 	pps_document_doc_mutex_unlock ();
 
 	pps_job_succeeded (job);
@@ -281,12 +281,12 @@ pps_job_annots_run (PpsJob *job)
 	g_debug ("running annots job");
 
 	pps_document_doc_mutex_lock ();
-	for (i = 0; i < pps_document_get_n_pages (job->document); i++) {
+	for (i = 0; i < pps_document_get_n_pages (pps_job_get_document (job)); i++) {
 		PpsMappingList *mapping_list;
 		PpsPage        *page;
 
-		page = pps_document_get_page (job->document, i);
-		mapping_list = pps_document_annotations_get_annotations (PPS_DOCUMENT_ANNOTATIONS (job->document),
+		page = pps_document_get_page (pps_job_get_document (job), i);
+		mapping_list = pps_document_annotations_get_annotations (PPS_DOCUMENT_ANNOTATIONS (pps_job_get_document (job)),
 									page);
 		g_object_unref (page);
 
@@ -387,13 +387,13 @@ pps_job_render_texture_run (PpsJob *job)
 
 	PPS_PROFILER_START (PPS_GET_TYPE_NAME (job), g_strdup_printf("page: %d", job_render->page));
 
-	pps_page = pps_document_get_page (job->document, job_render->page);
+	pps_page = pps_document_get_page (pps_job_get_document (job), job_render->page);
 	rc = pps_render_context_new (pps_page, job_render->rotation, job_render->scale);
 	pps_render_context_set_target_size (rc,
 					   job_render->target_width, job_render->target_height);
 	g_object_unref (pps_page);
 
-	surface = pps_document_render (job->document, rc);
+	surface = pps_document_render (pps_job_get_document (job), rc);
 
 	if (surface == NULL ||
 	    cairo_surface_status (surface) != CAIRO_STATUS_SUCCESS) {
@@ -427,7 +427,7 @@ pps_job_render_texture_run (PpsJob *job)
 	/* If job was cancelled during the page rendering,
 	 * we return now, so that the thread is finished ASAP
 	 */
-	if (g_cancellable_is_cancelled (job->cancellable)) {
+	if (g_cancellable_is_cancelled (pps_job_get_cancellable (job))) {
 		PPS_PROFILER_STOP ();
 		pps_document_fc_mutex_unlock ();
 		pps_document_doc_mutex_unlock ();
@@ -435,8 +435,8 @@ pps_job_render_texture_run (PpsJob *job)
 		return FALSE;
 	}
 
-	if (job_render->include_selection && PPS_IS_SELECTION (job->document)) {
-		pps_selection_render_selection (PPS_SELECTION (job->document),
+	if (job_render->include_selection && PPS_IS_SELECTION (pps_job_get_document (job))) {
+		pps_selection_render_selection (PPS_SELECTION (pps_job_get_document (job)),
 					       rc,
 					       &selection,
 					       &(job_render->selection_points),
@@ -444,7 +444,7 @@ pps_job_render_texture_run (PpsJob *job)
 					       job_render->selection_style,
 					       &(job_render->text), &(job_render->base));
 		job_render->selection_region =
-			pps_selection_get_selection_region (PPS_SELECTION (job->document),
+			pps_selection_get_selection_region (PPS_SELECTION (pps_job_get_document (job)),
 							   rc,
 							   job_render->selection_style,
 							   &(job_render->selection_points));
@@ -506,22 +506,22 @@ pps_job_page_data_run (PpsJob *job)
 	g_debug ("running page data job: page: %d (%p)", job_pd->page, job);
 
 	pps_document_doc_mutex_lock ();
-	pps_page = pps_document_get_page (job->document, job_pd->page);
+	pps_page = pps_document_get_page (pps_job_get_document (job), job_pd->page);
 
-	if ((job_pd->flags & PPS_PAGE_DATA_INCLUDE_TEXT_MAPPING) && PPS_IS_DOCUMENT_TEXT (job->document))
+	if ((job_pd->flags & PPS_PAGE_DATA_INCLUDE_TEXT_MAPPING) && PPS_IS_DOCUMENT_TEXT (pps_job_get_document (job)))
 		job_pd->text_mapping =
-			pps_document_text_get_text_mapping (PPS_DOCUMENT_TEXT (job->document), pps_page);
-	if ((job_pd->flags & PPS_PAGE_DATA_INCLUDE_TEXT) && PPS_IS_DOCUMENT_TEXT (job->document))
+			pps_document_text_get_text_mapping (PPS_DOCUMENT_TEXT (pps_job_get_document (job)), pps_page);
+	if ((job_pd->flags & PPS_PAGE_DATA_INCLUDE_TEXT) && PPS_IS_DOCUMENT_TEXT (pps_job_get_document (job)))
 		job_pd->text =
-			pps_document_text_get_text (PPS_DOCUMENT_TEXT (job->document), pps_page);
-	if ((job_pd->flags & PPS_PAGE_DATA_INCLUDE_TEXT_LAYOUT) && PPS_IS_DOCUMENT_TEXT (job->document))
-		pps_document_text_get_text_layout (PPS_DOCUMENT_TEXT (job->document),
+			pps_document_text_get_text (PPS_DOCUMENT_TEXT (pps_job_get_document (job)), pps_page);
+	if ((job_pd->flags & PPS_PAGE_DATA_INCLUDE_TEXT_LAYOUT) && PPS_IS_DOCUMENT_TEXT (pps_job_get_document (job)))
+		pps_document_text_get_text_layout (PPS_DOCUMENT_TEXT (pps_job_get_document (job)),
 						  pps_page,
 						  &(job_pd->text_layout),
 						  &(job_pd->text_layout_length));
-	if ((job_pd->flags & PPS_PAGE_DATA_INCLUDE_TEXT_ATTRS) && PPS_IS_DOCUMENT_TEXT (job->document))
+	if ((job_pd->flags & PPS_PAGE_DATA_INCLUDE_TEXT_ATTRS) && PPS_IS_DOCUMENT_TEXT (pps_job_get_document (job)))
 		job_pd ->text_attrs =
-			pps_document_text_get_text_attrs (PPS_DOCUMENT_TEXT (job->document),
+			pps_document_text_get_text_attrs (PPS_DOCUMENT_TEXT (pps_job_get_document (job)),
 							 pps_page);
         if ((job_pd->flags & PPS_PAGE_DATA_INCLUDE_TEXT_LOG_ATTRS) && job_pd->text) {
                 job_pd->text_log_attrs_length = g_utf8_strlen (job_pd->text, -1);
@@ -530,24 +530,24 @@ pps_job_page_data_run (PpsJob *job)
                 /* FIXME: We need API to get the language of the document */
                 pango_get_log_attrs (job_pd->text, -1, -1, NULL, job_pd->text_log_attrs, job_pd->text_log_attrs_length + 1);
         }
-	if ((job_pd->flags & PPS_PAGE_DATA_INCLUDE_LINKS) && PPS_IS_DOCUMENT_LINKS (job->document))
+	if ((job_pd->flags & PPS_PAGE_DATA_INCLUDE_LINKS) && PPS_IS_DOCUMENT_LINKS (pps_job_get_document (job)))
 		job_pd->link_mapping =
-			pps_document_links_get_links (PPS_DOCUMENT_LINKS (job->document), pps_page);
-	if ((job_pd->flags & PPS_PAGE_DATA_INCLUDE_FORMS) && PPS_IS_DOCUMENT_FORMS (job->document))
+			pps_document_links_get_links (PPS_DOCUMENT_LINKS (pps_job_get_document (job)), pps_page);
+	if ((job_pd->flags & PPS_PAGE_DATA_INCLUDE_FORMS) && PPS_IS_DOCUMENT_FORMS (pps_job_get_document (job)))
 		job_pd->form_field_mapping =
-			pps_document_forms_get_form_fields (PPS_DOCUMENT_FORMS (job->document),
+			pps_document_forms_get_form_fields (PPS_DOCUMENT_FORMS (pps_job_get_document (job)),
 							   pps_page);
-	if ((job_pd->flags & PPS_PAGE_DATA_INCLUDE_IMAGES) && PPS_IS_DOCUMENT_IMAGES (job->document))
+	if ((job_pd->flags & PPS_PAGE_DATA_INCLUDE_IMAGES) && PPS_IS_DOCUMENT_IMAGES (pps_job_get_document (job)))
 		job_pd->image_mapping =
-			pps_document_images_get_image_mapping (PPS_DOCUMENT_IMAGES (job->document),
+			pps_document_images_get_image_mapping (PPS_DOCUMENT_IMAGES (pps_job_get_document (job)),
 							      pps_page);
-	if ((job_pd->flags & PPS_PAGE_DATA_INCLUDE_ANNOTS) && PPS_IS_DOCUMENT_ANNOTATIONS (job->document))
+	if ((job_pd->flags & PPS_PAGE_DATA_INCLUDE_ANNOTS) && PPS_IS_DOCUMENT_ANNOTATIONS (pps_job_get_document (job)))
 		job_pd->annot_mapping =
-			pps_document_annotations_get_annotations (PPS_DOCUMENT_ANNOTATIONS (job->document),
+			pps_document_annotations_get_annotations (PPS_DOCUMENT_ANNOTATIONS (pps_job_get_document (job)),
 								 pps_page);
-        if ((job_pd->flags & PPS_PAGE_DATA_INCLUDE_MEDIA) && PPS_IS_DOCUMENT_MEDIA (job->document))
+        if ((job_pd->flags & PPS_PAGE_DATA_INCLUDE_MEDIA) && PPS_IS_DOCUMENT_MEDIA (pps_job_get_document (job)))
                 job_pd->media_mapping =
-                        pps_document_media_get_media_mapping (PPS_DOCUMENT_MEDIA (job->document),
+                        pps_document_media_get_media_mapping (PPS_DOCUMENT_MEDIA (pps_job_get_document (job)),
                                                              pps_page);
 	g_object_unref (pps_page);
 	pps_document_doc_mutex_unlock ();
@@ -615,13 +615,13 @@ pps_job_thumbnail_texture_run (PpsJob *job)
 	pps_document_doc_mutex_lock ();
 
 	PPS_PROFILER_START (PPS_GET_TYPE_NAME (job), g_strdup_printf("page: %d", job_thumb->page));
-	page = pps_document_get_page (job->document, job_thumb->page);
+	page = pps_document_get_page (pps_job_get_document (job), job_thumb->page);
 	rc = pps_render_context_new (page, job_thumb->rotation, job_thumb->scale);
 	pps_render_context_set_target_size (rc,
 					   job_thumb->target_width, job_thumb->target_height);
 	g_object_unref (page);
 
-	surface = pps_document_get_thumbnail_surface (job->document, rc);
+	surface = pps_document_get_thumbnail_surface (pps_job_get_document (job), rc);
 
 	job_thumb->thumbnail_texture = pps_document_misc_texture_from_surface (surface);
 	cairo_surface_destroy(surface);
@@ -840,9 +840,7 @@ pps_job_load_run (PpsJob *job)
 							   job_load->password);
 		}
 
-		job->failed = FALSE;
-		job->finished = FALSE;
-		g_clear_error (&job->error);
+		pps_job_reset (job);
 
 		if (job_load->uri) {
 			uncompressed_uri = g_object_get_data (G_OBJECT (loaded_doc),
@@ -861,7 +859,7 @@ pps_job_load_run (PpsJob *job)
 				pps_document_load_fd (loaded_doc,
 						     fd,
 						     job_load->flags,
-						     job->cancellable,
+						     pps_job_get_cancellable (job),
 						     &error);
 		}
 	} else {
@@ -881,7 +879,7 @@ pps_job_load_run (PpsJob *job)
 					pps_document_factory_get_document_for_fd (fd,
 										 job_load->mime_type,
 										 job_load->flags,
-										 job->cancellable,
+										 pps_job_get_cancellable (job),
 										 &error);
 		}
 	}
@@ -1127,7 +1125,7 @@ pps_job_save_run (PpsJob *job)
 	/* Save document to temp filename */
 	local_uri = g_filename_to_uri (tmp_filename, NULL, &error);
         if (local_uri != NULL) {
-                pps_document_save (job->document, local_uri, &error);
+                pps_document_save (pps_job_get_document (job), local_uri, &error);
         }
 
 	pps_document_doc_mutex_unlock ();
@@ -1143,7 +1141,7 @@ pps_job_save_run (PpsJob *job)
 	/* If original document was compressed,
 	 * compress it again before saving
 	 */
-	if (g_object_get_data (G_OBJECT (job->document), "uri-uncompressed")) {
+	if (g_object_get_data (G_OBJECT (pps_job_get_document (job)), "uri-uncompressed")) {
 		PpsCompressionType ctype = PPS_COMPRESSION_NONE;
 		const gchar      *ext;
 		gchar            *uri_comp;
@@ -1258,7 +1256,7 @@ static gboolean
 pps_job_find_run (PpsJob *job)
 {
 	PpsJobFind      *job_find = PPS_JOB_FIND (job);
-	PpsDocumentFind *find = PPS_DOCUMENT_FIND (job->document);
+	PpsDocumentFind *find = PPS_DOCUMENT_FIND (pps_job_get_document (job));
 	PpsPage         *pps_page;
 	GList           *matches;
 	gint             n_pages, current_page;
@@ -1269,11 +1267,11 @@ pps_job_find_run (PpsJob *job)
 	current_page = job_find->start_page;
 
 	while (n_pages-- > 0) {
-		if (g_cancellable_is_cancelled (job->cancellable))
+		if (g_cancellable_is_cancelled (pps_job_get_cancellable (job)))
 			return FALSE;
 
 		pps_document_doc_mutex_lock ();
-		pps_page = pps_document_get_page (job->document, current_page);
+		pps_page = pps_document_get_page (pps_job_get_document (job), current_page);
 		matches = pps_document_find_find_text (find, pps_page, job_find->text,
 						       job_find->options);
 		g_object_unref (pps_page);
@@ -1418,7 +1416,7 @@ pps_job_layers_run (PpsJob *job)
 	g_debug ("running layers job");
 
 	pps_document_doc_mutex_lock ();
-	job_layers->model = pps_document_layers_get_layers (PPS_DOCUMENT_LAYERS (job->document));
+	job_layers->model = pps_document_layers_get_layers (PPS_DOCUMENT_LAYERS (pps_job_get_document (job)));
 	pps_document_doc_mutex_unlock ();
 
 	pps_job_succeeded (job);
@@ -1492,11 +1490,9 @@ pps_job_export_run (PpsJob *job)
 
 	pps_document_doc_mutex_lock ();
 
-	pps_page = pps_document_get_page (job->document, job_export->page);
+	pps_page = pps_document_get_page (pps_job_get_document (job), job_export->page);
 	if (job_export->rc) {
-		job->failed = FALSE;
-		job->finished = FALSE;
-		g_clear_error (&job->error);
+		pps_job_reset (job);
 
 		pps_render_context_set_page (job_export->rc, pps_page);
 	} else {
@@ -1504,7 +1500,7 @@ pps_job_export_run (PpsJob *job)
 	}
 	g_object_unref (pps_page);
 
-	pps_file_exporter_do_page (PPS_FILE_EXPORTER (job->document), job_export->rc);
+	pps_file_exporter_do_page (PPS_FILE_EXPORTER (pps_job_get_document (job)), job_export->rc);
 
 	pps_document_doc_mutex_unlock ();
 
@@ -1574,20 +1570,18 @@ pps_job_print_run (PpsJob *job)
 
 	g_debug ("running print job");
 
-	job->failed = FALSE;
-	job->finished = FALSE;
-	g_clear_error (&job->error);
+	pps_job_reset (job);
 
 	pps_document_doc_mutex_lock ();
 
-	pps_page = pps_document_get_page (job->document, job_print->page);
-	pps_document_print_print_page (PPS_DOCUMENT_PRINT (job->document),
+	pps_page = pps_document_get_page (pps_job_get_document (job), job_print->page);
+	pps_document_print_print_page (PPS_DOCUMENT_PRINT (pps_job_get_document (job)),
 				      pps_page, job_print->cr);
 	g_object_unref (pps_page);
 
 	pps_document_doc_mutex_unlock ();
 
-        if (g_cancellable_is_cancelled (job->cancellable))
+        if (g_cancellable_is_cancelled (pps_job_get_cancellable (job)))
                 return FALSE;
 
 	cr_status = cairo_status (job_print->cr);

shell-rs/papers-view/sys/Gir.toml
@@ -14,5 +14,6 @@ external_libraries = [
     "Cairo",
     "Gdk",
     "Gtk",
+    "Pango",
     "PapersDocument"
 ]

commit 13dcf088b201ca3446feefe92aee3aec653cb4c5

libview: Fix stalled print previewer
Fixes: fec45cabf ("libview: Switch to use private class member for PpsJob")

Signed-off-by: Qiu Wenbo <qiuwenbo@gnome.org>

libview/pps-print-operation.c
@@ -2530,7 +2530,7 @@ pps_print_operation_print_draw_page (PpsPrintOperationPrint *print,
                 g_signal_connect (G_OBJECT (print->job_print), "cancelled",
                                   G_CALLBACK (print_job_cancelled),
                                   (gpointer)print);
-	} else if (pps_job_get_cancellable (print->job_print)) {
+	} else if (g_cancellable_is_cancelled (pps_job_get_cancellable (print->job_print))) {
                 gtk_print_operation_cancel (print->op);
                 pps_job_print_set_cairo (PPS_JOB_PRINT (print->job_print), NULL);
                 return;