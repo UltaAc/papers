142b1cc0a21505f01d958d5a486ffd5f73fb4e67
Pablo Correa Gómez <pabloyoyoista@postmarketos.org>
2024-12-01 18:00:38 +0100

    libview: improve memory management around attachment context's result model
    Init it at init time instead of at get time, and clean up its memory
    at dispose time. This surfaces a small bug, that could lead to race
    condition. If the job ever finishes running before "get_model" is
    called, then we would have ended in a Segmentation Fault

--- libview/context/pps-attachment-context.c
+++ libview/context/pps-attachment-context.c
@@ -135,8 +135,10 @@ static void
 pps_attachment_context_dispose (GObject *object)
 {
 	PpsAttachmentContext *context = PPS_ATTACHMENT_CONTEXT (object);
+	PpsAttachmentContextPrivate *priv = GET_PRIVATE (context);
 
 	pps_attachment_context_clear_job (context);
+	g_clear_object (&priv->attachment_model);
 
 	G_OBJECT_CLASS (pps_attachment_context_parent_class)->dispose (object);
 }
@@ -144,6 +146,9 @@ pps_attachment_context_dispose (GObject *object)
 static void
 pps_attachment_context_init (PpsAttachmentContext *context)
 {
+	PpsAttachmentContextPrivate *priv = GET_PRIVATE (context);
+
+	priv->attachment_model = g_list_store_new (PPS_TYPE_ATTACHMENT);
 }
 
 static void
@@ -217,10 +222,6 @@ GListModel *
 pps_attachment_context_get_model (PpsAttachmentContext *context)
 {
 	PpsAttachmentContextPrivate *priv = GET_PRIVATE (context);
-
-	if (priv->attachment_model == NULL) {
-		priv->attachment_model = g_list_store_new (PPS_TYPE_ATTACHMENT);
-	}
 
 	return G_LIST_MODEL (priv->attachment_model);
 }
