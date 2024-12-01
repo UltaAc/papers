UltaAc/papers

commit fe6334c1db5306d7790faaa8003e3190700703a6

libdocument: remove cancellable from pps_document_load_fd
It's unused, and all these operations are synchronous across the
application. So it makes no sense to do like if they were cancellable

libdocument/pps-document-factory.c
@@ -435,7 +435,6 @@ PpsDocument *
 pps_document_factory_get_document_for_fd (int fd,
                                           const char *mime_type,
                                           PpsDocumentLoadFlags flags,
-                                          GCancellable *cancellable,
                                           GError **error)
 {
 	PpsDocument *document;
@@ -456,7 +455,7 @@ pps_document_factory_get_document_for_fd (int fd,
 		return NULL;
 	}
 
-	if (!pps_document_load_fd (document, fd, flags, cancellable, error)) {
+	if (!pps_document_load_fd (document, fd, flags, error)) {
 		/* fd is now consumed */
 		g_object_unref (document);
 		return NULL;

libdocument/backend/pdf/pps-poppler.c
@@ -310,7 +310,6 @@ static gboolean
 pdf_document_load_fd (PpsDocument *document,
                       int fd,
                       PpsDocumentLoadFlags flags,
-                      GCancellable *cancellable,
                       GError **error)
 {
 	GError *err = NULL;

libdocument/pps-document-factory.h
@@ -43,7 +43,6 @@ PPS_PUBLIC
 PpsDocument *pps_document_factory_get_document_for_fd (int fd,
                                                        const char *mime_type,
                                                        PpsDocumentLoadFlags flags,
-                                                       GCancellable *cancellable,
                                                        GError **error);
 
 PPS_PUBLIC

libdocument/pps-document.c
@@ -432,7 +432,6 @@ pps_document_load (PpsDocument *document,
  * @document: a #PpsDocument
  * @fd: a file descriptor
  * @flags: flags from #PpsDocumentLoadFlags
- * @cancellable: (allow-none): a #GCancellable, or %NULL
  * @error: (allow-none): a #GError location to store an error, or %NULL
  *
  * Synchronously loads the document from @fd, which must refer to
@@ -452,7 +451,6 @@ gboolean
 pps_document_load_fd (PpsDocument *document,
                       int fd,
                       PpsDocumentLoadFlags flags,
-                      GCancellable *cancellable,
                       GError **error)
 {
 	PpsDocumentClass *klass;
@@ -461,7 +459,6 @@ pps_document_load_fd (PpsDocument *document,
 
 	g_return_val_if_fail (PPS_IS_DOCUMENT (document), FALSE);
 	g_return_val_if_fail (fd != -1, FALSE);
-	g_return_val_if_fail (cancellable == NULL || G_IS_CANCELLABLE (cancellable), FALSE);
 	g_return_val_if_fail (error == NULL || *error == NULL, FALSE);
 
 	klass = PPS_DOCUMENT_GET_CLASS (document);
@@ -501,7 +498,7 @@ pps_document_load_fd (PpsDocument *document,
 		return FALSE;
 	}
 
-	if (!klass->load_fd (document, fd, flags, cancellable, error))
+	if (!klass->load_fd (document, fd, flags, error))
 		return FALSE;
 
 	if (!(flags & PPS_DOCUMENT_LOAD_FLAG_NO_CACHE))

libdocument/pps-document.h
@@ -113,7 +113,6 @@ struct _PpsDocumentClass {
 	gboolean (*load_fd) (PpsDocument *document,
 	                     int fd,
 	                     PpsDocumentLoadFlags flags,
-	                     GCancellable *cancellable,
 	                     GError **error);
 };
 
@@ -151,7 +150,6 @@ PPS_PUBLIC
 gboolean pps_document_load_fd (PpsDocument *document,
                                int fd,
                                PpsDocumentLoadFlags flags,
-                               GCancellable *cancellable,
                                GError **error);
 PPS_PUBLIC
 gboolean pps_document_save (PpsDocument *document,

libview/pps-jobs.c
@@ -872,7 +872,6 @@ pps_job_load_run (PpsJob *job)
 				pps_document_load_fd (loaded_doc,
 				                      fd,
 				                      priv->flags,
-				                      pps_job_get_cancellable (job),
 				                      &error);
 		}
 	} else {
@@ -892,7 +891,6 @@ pps_job_load_run (PpsJob *job)
 				    pps_document_factory_get_document_for_fd (fd,
 				                                              priv->mime_type,
 				                                              priv->flags,
-				                                              pps_job_get_cancellable (job),
 				                                              &error);
 		}
 	}

commit 782b56c0681084382397c00df5b23ee73fd1308e

libdocument: remove cancellable from pps_document_load_fd doc comments
Fixes: fe6334c1d ("libdocument: remove cancellable from pps_document_load_fd")

libdocument/pps-document-factory.c
@@ -410,7 +410,6 @@ pps_document_factory_get_document (const char *uri, GError **error)
  * @fd: a file descriptor
  * @mime_type: the mime type
  * @flags: flags from #PpsDocumentLoadFlags
- * @cancellable: (allow-none): a #GCancellable, or %NULL
  * @error: (allow-none): a #GError location to store an error, or %NULL
  *
  * Synchronously creates a #PpsDocument for the document from @fd using the backend