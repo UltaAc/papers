fc0a6cd02b7a831994ca5def41c39346dd97b91f
Qiu Wenbo <qiuwenbo@kylinos.com.cn>
2024-12-20 18:35:27 +0800

    shell: Remove PpsMessageArea
    Signed-off-by: Qiu Wenbo <qiuwenbo@kylinos.com.cn>

--- shell/pps-progress-message-area.c
+++ shell/pps-progress-message-area.c
@@ -1,7 +1,7 @@
-/* pps-progress-message-area.c
+/* pps-message-area.c
  *  this file is part of papers, a gnome document viewer
  *
- * Copyright (C) 2008 Carlos Garcia Campos <carlosgc@gnome.org>
+ * Copyright (C) 2007 Carlos Garcia Campos <carlosgc@gnome.org>
  * Copyright (C) 2018 Germán Poo-Caamaño <gpoo@gnome.org>
  *
  * Papers is free software; you can redistribute it and/or modify it
@@ -21,33 +21,38 @@
 
 #include <config.h>
 
-#include <gtk/gtk.h>
-
-#include "pps-message-area.h"
 #include "pps-progress-message-area.h"
 
 typedef struct
 {
+	GtkInfoBar *info_bar;
+	GtkWidget *image;
 	GtkWidget *label;
+	GtkWidget *secondary_label;
 	GtkWidget *progress_bar;
+	GtkWidget *progress_label;
 } PpsProgressMessageAreaPrivate;
 
 enum {
 	PROP_0,
+	PROP_TEXT,
+	PROP_SECONDARY_TEXT,
 	PROP_STATUS,
-	PROP_FRACTION
+	PROP_FRACTION,
 };
 
 static void pps_progress_message_area_set_property (GObject *object,
-                                                    guint prop_id,
-                                                    const GValue *value,
-                                                    GParamSpec *pspec);
+                                           guint prop_id,
+                                           const GValue *value,
+                                           GParamSpec *pspec);
 static void pps_progress_message_area_get_property (GObject *object,
-                                                    guint prop_id,
-                                                    GValue *value,
-                                                    GParamSpec *pspec);
+                                           guint prop_id,
+                                           GValue *value,
+                                           GParamSpec *pspec);
 
-G_DEFINE_TYPE_WITH_PRIVATE (PpsProgressMessageArea, pps_progress_message_area, PPS_TYPE_MESSAGE_AREA)
+G_DEFINE_TYPE_WITH_PRIVATE (PpsProgressMessageArea, pps_progress_message_area, ADW_TYPE_BIN)
+
+#define GET_PRIVATE(o) pps_progress_message_area_get_instance_private (o)
 
 static void
 pps_progress_message_area_class_init (PpsProgressMessageAreaClass *class)
@@ -59,8 +64,31 @@ pps_progress_message_area_class_init (PpsProgressMessageAreaClass *class)
 	gobject_class->get_property = pps_progress_message_area_get_property;
 
 	gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/papers/ui/progress-message-area.ui");
+	gtk_widget_class_bind_template_child_private (widget_class, PpsProgressMessageArea, info_bar);
+	gtk_widget_class_bind_template_child_private (widget_class, PpsProgressMessageArea, image);
 	gtk_widget_class_bind_template_child_private (widget_class, PpsProgressMessageArea, label);
+	gtk_widget_class_bind_template_child_private (widget_class, PpsProgressMessageArea, secondary_label);
 	gtk_widget_class_bind_template_child_private (widget_class, PpsProgressMessageArea, progress_bar);
+	gtk_widget_class_bind_template_child_private (widget_class, PpsProgressMessageArea, progress_label);
+
+	gtk_widget_class_set_accessible_role (widget_class, GTK_ACCESSIBLE_ROLE_ALERT);
+
+	g_object_class_install_property (gobject_class,
+	                                 PROP_TEXT,
+	                                 g_param_spec_string ("text",
+	                                                      "Text",
+	                                                      "The primary text of the message dialog",
+	                                                      NULL,
+	                                                      G_PARAM_READWRITE |
+	                                                          G_PARAM_STATIC_STRINGS));
+	g_object_class_install_property (gobject_class,
+	                                 PROP_SECONDARY_TEXT,
+	                                 g_param_spec_string ("secondary-text",
+	                                                      "Secondary Text",
+	                                                      "The secondary text of the message dialog",
+	                                                      NULL,
+	                                                      G_PARAM_READWRITE |
+	                                                          G_PARAM_STATIC_STRINGS));
 
 	g_object_class_install_property (gobject_class,
 	                                 PROP_STATUS,
@@ -88,13 +116,19 @@ pps_progress_message_area_init (PpsProgressMessageArea *area)
 
 static void
 pps_progress_message_area_set_property (GObject *object,
-                                        guint prop_id,
-                                        const GValue *value,
-                                        GParamSpec *pspec)
+                               guint prop_id,
+                               const GValue *value,
+                               GParamSpec *pspec)
 {
 	PpsProgressMessageArea *area = PPS_PROGRESS_MESSAGE_AREA (object);
 
 	switch (prop_id) {
+	case PROP_TEXT:
+		pps_progress_message_area_set_text (area, g_value_get_string (value));
+		break;
+	case PROP_SECONDARY_TEXT:
+		pps_progress_message_area_set_secondary_text (area, g_value_get_string (value));
+		break;
 	case PROP_STATUS:
 		pps_progress_message_area_set_status (area, g_value_get_string (value));
 		break;
@@ -103,65 +137,140 @@ pps_progress_message_area_set_property (GObject *object,
 		break;
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+		break;
 	}
 }
 
 static void
 pps_progress_message_area_get_property (GObject *object,
-                                        guint prop_id,
-                                        GValue *value,
-                                        GParamSpec *pspec)
+                               guint prop_id,
+                               GValue *value,
+                               GParamSpec *pspec)
 {
 	PpsProgressMessageArea *area = PPS_PROGRESS_MESSAGE_AREA (object);
-	PpsProgressMessageAreaPrivate *priv;
-
-	priv = pps_progress_message_area_get_instance_private (area);
+	PpsProgressMessageAreaPrivate *priv = GET_PRIVATE (area);
 
 	switch (prop_id) {
-	case PROP_STATUS:
+	case PROP_TEXT:
 		g_value_set_string (value, gtk_label_get_label (GTK_LABEL (priv->label)));
+		break;
+	case PROP_SECONDARY_TEXT:
+		g_value_set_string (value, gtk_label_get_label (GTK_LABEL (priv->secondary_label)));
+		break;
+	case PROP_STATUS:
+		g_value_set_string (value, gtk_label_get_label (GTK_LABEL (priv->progress_label)));
 		break;
 	case PROP_FRACTION: {
 		gdouble fraction;
 
 		fraction = gtk_progress_bar_get_fraction (GTK_PROGRESS_BAR (priv->progress_bar));
 		g_value_set_double (value, fraction);
-	} break;
+		break;
+	}
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+		break;
 	}
 }
 
-GtkWidget *
-pps_progress_message_area_new (const gchar *icon_name,
-                               const gchar *text)
+void
+pps_progress_message_area_add_button (PpsProgressMessageArea *area,
+                             const gchar *button_text,
+                             gint response_id)
 {
-	GtkWidget *widget;
-	GtkInfoBar *info_bar;
+	PpsProgressMessageAreaPrivate *priv = GET_PRIVATE (area);
 
-	widget = g_object_new (PPS_TYPE_PROGRESS_MESSAGE_AREA,
-	                       "text", text,
-	                       NULL);
-	info_bar = pps_message_area_get_info_bar (PPS_MESSAGE_AREA (widget));
-	gtk_info_bar_set_message_type (info_bar, GTK_MESSAGE_OTHER);
+	if (button_text == NULL)
+		return;
 
-	pps_message_area_set_image_from_icon_name (PPS_MESSAGE_AREA (widget),
-	                                           icon_name);
+	gtk_info_bar_add_button (priv->info_bar, button_text, response_id);
+}
 
-	return widget;
+/**
+ * pps_progress_message_area_get_info_bar
+ * @area: A #PpsProgressMessageArea
+ *
+ * Returns: (transfer none) (not nullable): The #GtkInfoBar
+ */
+GtkInfoBar *
+pps_progress_message_area_get_info_bar (PpsProgressMessageArea *area)
+{
+	PpsProgressMessageAreaPrivate *priv = GET_PRIVATE (area);
+
+	return priv->info_bar;
+}
+
+void
+pps_progress_message_area_set_image_from_icon_name (PpsProgressMessageArea *area,
+                                           const gchar *icon_name)
+{
+	PpsProgressMessageAreaPrivate *priv = GET_PRIVATE (area);
+
+	g_return_if_fail (PPS_IS_PROGRESS_MESSAGE_AREA (area));
+	g_return_if_fail (icon_name != NULL);
+
+	gtk_image_set_from_icon_name (GTK_IMAGE (priv->image),
+	                              icon_name);
+}
+
+void
+pps_progress_message_area_set_text (PpsProgressMessageArea *area,
+                           const gchar *str)
+{
+	PpsProgressMessageAreaPrivate *priv;
+
+	g_return_if_fail (PPS_IS_PROGRESS_MESSAGE_AREA (area));
+
+	priv = GET_PRIVATE (area);
+
+	if (str) {
+		gchar *msg, *escaped;
+
+		escaped = g_markup_escape_text (str, -1);
+		msg = g_strdup_printf ("<b>%s</b>", escaped);
+		gtk_label_set_markup (GTK_LABEL (priv->label), msg);
+		g_free (msg);
+		g_free (escaped);
+	} else {
+		gtk_label_set_markup (GTK_LABEL (priv->label), NULL);
+	}
+
+	g_object_notify (G_OBJECT (area), "text");
+}
+
+void
+pps_progress_message_area_set_secondary_text (PpsProgressMessageArea *area,
+                                     const gchar *str)
+{
+	PpsProgressMessageAreaPrivate *priv;
+
+	g_return_if_fail (PPS_IS_PROGRESS_MESSAGE_AREA (area));
+
+	priv = GET_PRIVATE (area);
+
+	if (str) {
+		gchar *msg;
+
+		msg = g_strdup_printf ("<small>%s</small>", str);
+		gtk_label_set_markup (GTK_LABEL (priv->secondary_label), msg);
+		g_free (msg);
+	} else {
+		gtk_label_set_markup (GTK_LABEL (priv->secondary_label), NULL);
+	}
+
+	gtk_widget_set_visible (priv->secondary_label, str != NULL);
+
+	g_object_notify (G_OBJECT (area), "secondary-text");
 }
 
 void
 pps_progress_message_area_set_status (PpsProgressMessageArea *area,
                                       const gchar *str)
 {
-	PpsProgressMessageAreaPrivate *priv;
-
+	PpsProgressMessageAreaPrivate *priv = GET_PRIVATE (area);
 	g_return_if_fail (PPS_IS_PROGRESS_MESSAGE_AREA (area));
 
-	priv = pps_progress_message_area_get_instance_private (area);
-
-	gtk_label_set_text (GTK_LABEL (priv->label), str);
+	gtk_label_set_text (GTK_LABEL (priv->progress_label), str);
 
 	g_object_notify (G_OBJECT (area), "status");
 }
@@ -170,13 +279,26 @@ void
 pps_progress_message_area_set_fraction (PpsProgressMessageArea *area,
                                         gdouble fraction)
 {
-	PpsProgressMessageAreaPrivate *priv;
-
+	PpsProgressMessageAreaPrivate *priv = GET_PRIVATE (area);
 	g_return_if_fail (PPS_IS_PROGRESS_MESSAGE_AREA (area));
-
-	priv = pps_progress_message_area_get_instance_private (area);
 
 	gtk_progress_bar_set_fraction (GTK_PROGRESS_BAR (priv->progress_bar),
 	                               fraction);
 	g_object_notify (G_OBJECT (area), "fraction");
+}
+
+GtkWidget *
+pps_progress_message_area_new (const gchar *icon_name,
+                               const gchar *text)
+{
+	GtkWidget *widget;
+
+	widget = g_object_new (PPS_TYPE_PROGRESS_MESSAGE_AREA,
+	                       "text", text,
+	                       NULL);
+
+	pps_progress_message_area_set_image_from_icon_name (PPS_PROGRESS_MESSAGE_AREA (widget),
+	                                           icon_name);
+
+	return widget;
 }
--- shell/pps-progress-message-area.h
+++ shell/pps-progress-message-area.h
@@ -1,7 +1,7 @@
-/* pps-progress-message-area.h
+/* pps-message-area.h
  *  this file is part of papers, a gnome document viewer
  *
- * Copyright (C) 2008 Carlos Garcia Campos <carlosgc@gnome.org>
+ * Copyright (C) 2007 Carlos Garcia Campos <carlosgc@gnome.org>
  * Copyright (C) 2018 Germán Poo-Caamaño <gpoo@gnome.org>
  *
  * Papers is free software; you can redistribute it and/or modify it
@@ -21,33 +21,31 @@
 
 #pragma once
 
+#include <adwaita.h>
 #include <gtk/gtk.h>
-
-#include "pps-message-area.h"
 
 G_BEGIN_DECLS
 
 #define PPS_TYPE_PROGRESS_MESSAGE_AREA (pps_progress_message_area_get_type ())
-#define PPS_PROGRESS_MESSAGE_AREA(obj) (G_TYPE_CHECK_INSTANCE_CAST ((obj), PPS_TYPE_PROGRESS_MESSAGE_AREA, PpsProgressMessageArea))
-#define PPS_PROGRESS_MESSAGE_AREA_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST ((klass), PPS_TYPE_PROGRESS_MESSAGE_AREA, PpsProgressMessageAreaClass))
-#define PPS_IS_PROGRESS_MESSAGE_AREA(obj) (G_TYPE_CHECK_INSTANCE_TYPE ((obj), PPS_TYPE_PROGRESS_MESSAGE_AREA))
-#define PPS_IS_PROGRESS_MESSAGE_AREA_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), PPS_TYPE_PROGRESS_MESSAGE_AREA))
-#define PPS_PROGRESS_MESSAGE_AREA_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS ((obj), PPS_TYPE_PROGRESS_MESSAGE_AREA, PpsProgressMessageAreaClass))
-
-typedef struct _PpsProgressMessageArea PpsProgressMessageArea;
-typedef struct _PpsProgressMessageAreaClass PpsProgressMessageAreaClass;
+G_DECLARE_FINAL_TYPE(PpsProgressMessageArea, pps_progress_message_area, PPS, PROGRESS_MESSAGE_AREA, AdwBin)
 
 struct _PpsProgressMessageArea {
-	PpsMessageArea parent_instance;
+	AdwBin parent_instance;
 };
 
-struct _PpsProgressMessageAreaClass {
-	PpsMessageAreaClass parent_class;
-};
+GtkWidget *pps_progress_message_area_new (const gchar *icon_name, const gchar *text);
+void pps_progress_message_area_set_image_from_icon_name (PpsProgressMessageArea *area,
+                                                const gchar *icon_name);
+void pps_progress_message_area_set_text (PpsProgressMessageArea *area,
+                                const gchar *str);
+void pps_progress_message_area_set_secondary_text (PpsProgressMessageArea *area,
+                                          const gchar *str);
 
-GType pps_progress_message_area_get_type (void) G_GNUC_CONST;
-GtkWidget *pps_progress_message_area_new (const gchar *icon_name,
-                                          const gchar *text);
+void pps_progress_message_area_add_button (PpsProgressMessageArea *area,
+                                  const gchar *first_button_text,
+                                  gint response_id);
+GtkInfoBar *pps_progress_message_area_get_info_bar (PpsProgressMessageArea *area);
+
 void pps_progress_message_area_set_status (PpsProgressMessageArea *area,
                                            const gchar *str);
 void pps_progress_message_area_set_fraction (PpsProgressMessageArea *area,
